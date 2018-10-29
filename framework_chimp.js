9a4dcb250b29964195f64c25ea8a2ffc9bc1a572
xywang68 <xywang68@gmail.com>
2018-10-29 15:47:15 -0700

    add framework chimp.js

---
+++ framework_chimp.js
@@ -0,0 +1,30 @@
+const myFrameworkPath = process.env.FrameworkPath;
+require(myFrameworkPath + '/global/support/env.js');
+
+if (process.env.LOCALSELPORT) {
+  module.exports = require(myFrameworkPath + "/global/configs/chimp_local.js");
+} else {
+  switch (process.env.PLATFORM) {
+    case "Win10":
+      switch (process.env.BROWSER) {
+        case "IE":
+          process.env.BROWSER = 'IE';
+          module.exports = require(myFrameworkPath + "/global/configs/chimp_Win10_IE.js");
+        break;
+        case "CH":
+          module.exports = require(myFrameworkPath + "/global/configs/chimp_Win10_CH.js");
+        break;
+      }
+      break;
+    case "Linux":
+      switch(process.env.BROWSER) {
+        case "CH":
+          module.exports = require(myFrameworkPath + "/global/configs/chimp_Linux_CH.js");
+        break;
+      }
+    break;
+    default:
+      module.exports = require(myFrameworkPath + "/global/configs/chimp_Linux_CH.js");
+    break;
+  }
+}
