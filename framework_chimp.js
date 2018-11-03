e1ffe680004322ff0fdf3c0f67224e230792da95
xywang68 <xywang68@gmail.com>
2018-11-03 00:57:26 -0700

    update path

--- framework_chimp.js
+++ framework_chimp.js
@@ -1,33 +1,33 @@
 const myFrameworkPath = process.env.FrameworkPath;
-require(myFrameworkPath + '/global/support/env.js');
+require(myFrameworkPath + '/framework/support/framework_env.js');
 
 if (process.env.LOCALSELPORT) {
-  module.exports = require(myFrameworkPath + "/global/configs/chimp_local.js");
+  module.exports = require(myFrameworkPath + "/framework/configs/chimp_local.js");
 } else {
   switch (process.env.PLATFORM) {
     case "Win10":
     case "Win7":
       switch (process.env.BROWSER) {
         case "IE":
-          module.exports = require(myFrameworkPath + "/global/configs/chimp_Win10_IE.js");
+          module.exports = require(myFrameworkPath + "/framework/configs/chimp_Win10_IE.js");
         break;
         case "EDGE":
-          module.exports = require(myFrameworkPath + "/global/configs/chimp_Win10_EDGE.js");
+          module.exports = require(myFrameworkPath + "/framework/configs/chimp_Win10_EDGE.js");
         break;
         case "CH":
-          module.exports = require(myFrameworkPath + "/global/configs/chimp_Win10_CH.js");
+          module.exports = require(myFrameworkPath + "/framework/configs/chimp_Win10_CH.js");
         break;
       }
       break;
     case "Linux":
       switch(process.env.BROWSER) {
         case "CH":
-          module.exports = require(myFrameworkPath + "/global/configs/chimp_Linux_CH.js");
+          module.exports = require(myFrameworkPath + "/framework/configs/chimp_Linux_CH.js");
         break;
       }
     break;
     default:
-      module.exports = require(myFrameworkPath + "/global/configs/chimp_Linux_CH.js");
+      module.exports = require(myFrameworkPath + "/framework/configs/chimp_Linux_CH.js");
     break;
   }
 }
