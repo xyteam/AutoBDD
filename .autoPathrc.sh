cc740c124a77325513eab809c5ac15271838e95a
xywang68 <xywang68@gmail.com>
2018-11-07 14:42:20 -0800

    improve conditional assignment; update manual sync alias command

--- .autoPathrc.sh
+++ .autoPathrc.sh
@@ -5,5 +5,10 @@ export FrameworkPath=$(pwd)
 PATH=$(echo "${PATH//:/$'\n'}" | grep -v node_modules\/.bin | paste -sd ":" -)
 
 # assign current node_modules/.bin to PATH
-export PATH=$(./node_modules/.bin/npm-path)
+if [ -f ./node_modules/.bin/npm-path ]; then
+  export PATH=$(./node_modules/.bin/npm-path)
+fi
 
+# create command alias to sync between src and run env
+alias spr="rsync --archive --exclude .git/ --exclude node_modules/ $HOME/Projects/AutoBDD $HOME/Projects/xySikulixApi $HOME/Run"
+alias srp="rsync --archive --exclude .git/ --exclude node_modules/ $HOME/Run $HOME/Projects"
