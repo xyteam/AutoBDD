6acd5826581f18dc9a4e17085c1bf883d79171ff
xywang68 <xywang68@gmail.com>
2018-11-07 22:30:44 -0800

    add debug message to test framework, global and local support codes. All working

--- test-projects/Proto/Examples/features/step_definitions/When_I_visit_#.js
+++ test-projects/Proto/Examples/features/step_definitions/When_I_visit_#.js
@@ -1,5 +1,11 @@
 module.exports = function() {
   this.Given(/^I visit "([^"]*)"$/, {timeout: process.env.StepTimeoutInMS}, function (url) {
+    // debug messages
+    console.log(process.env.FrameworkEnv);
+    console.log(process.env.GlobalEnv);
+    console.log(this.frameworkVar);
+    console.log(this.globalVar);
+    // code starts here
     browser.url(url);
   });
 };
