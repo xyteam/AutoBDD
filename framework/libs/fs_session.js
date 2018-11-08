14b282570e3c1b4d944d9af006178767815a9fa4
xywang68 <xywang68@gmail.com>
2018-11-08 12:48:41 -0800

    add fs_session file

---
+++ framework/libs/fs_session.js
@@ -0,0 +1,8 @@
+// screen_session.js provides functions to read and write the Downlaods folder of the target system (local or remote)
+
+const FrameworkPath = process.env.FrameworkPath || process.env.HOME + '/Projects/AutoBDD';
+const execSync = require('child_process').execSync;
+
+module.exports = {
+
+}
