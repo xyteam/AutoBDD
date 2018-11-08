7afe47f169d6e5602dec6db625fec1c3d085ce33
xywang68 <xywang68@gmail.com>
2018-11-08 12:47:25 -0800

    add function to file

--- framework/libs/browser_session.js
+++ framework/libs/browser_session.js
@@ -1,4 +1,6 @@
-const robot_session = require('./robot_session');
+// browser_session.js provides additional functions to see and control the browser
+
+const screen_session = require('./screen_session');
 
 module.exports = {
   resetAll: function(session) {
@@ -31,11 +33,22 @@ module.exports = {
     session.newWindow(url);
     session.pause(1000);
     if (process.env.BROWSER == 'IE') {
-      robot_session.clickImage(null, 'IE_AllowOnce.png');
+      screen_session.clickImage(null, 'IE_AllowOnce.png');
       session.pause(3000);
-      robot_session.clickImage(null, 'IE_DismissX.png');
+      screen_session.clickImage(null, 'IE_DismissX.png');
     }
     session.pause(1000);
     clearInterval(handle);
+  },
+  
+  clickAndEnter: function(session, linkToClick) {
+    session.waitForExist(linkToClick, 15000);
+    try {
+      session.click(linkToClick)
+      try {
+        session.pause(1000);
+      } catch(e) {}
+    } catch(e) {}
+    screen_session.keyTap('enter');
   }
 }
