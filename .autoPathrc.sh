5cb609186eeab287b4b312ab8ac6a0c12f4ad351
xywang68 <xywang68@gmail.com>
2018-11-27 00:15:32 -0800

    move rsync aliase to xyPlatform userEnv.sh

--- .autoPathrc.sh
+++ .autoPathrc.sh
@@ -8,7 +8,3 @@ PATH=$(echo "${PATH//:/$'\n'}" | grep -v node_modules\/.bin | paste -sd ":" -)
 if [ -f ./node_modules/.bin/npm-path ]; then
   export PATH=$(./node_modules/.bin/npm-path)
 fi
-
-# create command alias to sync between src and run env
-alias spr="rsync --archive --exclude .git/ --exclude node_modules/ $HOME/Projects/AutoBDD $HOME/Projects/xySikulixApi $HOME/Run"
-alias srp="rsync --archive --exclude .git/ --exclude node_modules/ $HOME/Run $HOME/Projects"
