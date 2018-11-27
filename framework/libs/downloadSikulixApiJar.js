dff6c6100fe5f20f1749ee78d1fdd031c8eeafbd
xywang68 <xywang68@gmail.com>
2018-11-27 00:16:39 -0800

    use sikulix1pi 1.1.4 as default

--- framework/libs/downloadSikulixApiJar.js
+++ framework/libs/downloadSikulixApiJar.js
@@ -2,8 +2,10 @@
 
 var argv = require('minimist')(process.argv.slice(2));
 const FrameworkPath = process.env.FrameworkPath || process.env.HOME + '/Projects/AutoBDD';
-const sikuliApiJar = argv.sikulixApiJar || FrameworkPath + '/framework/libs/sikulixapi-latest.jar';
-const sikuliApiUrl = argv.sikulixUrl || 'https://raiman.github.io/SikuliX1/sikulixapi.jar';
+const sikuliApiJar = argv.sikulixApiJar || FrameworkPath + '/framework/libs/sikulixapi-1.1.4.jar';
+const sikuliApiUrl_latest = 'https://raiman.github.io/SikuliX1/sikulixapi.jar';
+const sikuliApiUrl_114 = 'https://raiman.github.io/SikuliX1/sikulixapi.jar';
+var sikuliApiUrl = argv.sikulixUrl || sikuliApiUrl_114;
 const fs = require('fs');
 const https = require('https');
 const java = require('java');
