e36f781c7069f4244e52f99b8c73914ef0bd7e21
xywang68 <xywang68@gmail.com>
2018-11-27 18:52:48 -0800

    refine feature and step file

--- test-projects/Proto/Examples/features/step_definitions/Then/Then_I_should_see_the_image_of_#_on_the_page.js
+++ test-projects/Proto/Examples/features/step_definitions/Then/Then_I_should_see_the_image_of_#_on_the_page.js
@@ -1,15 +1,10 @@
 module.exports = function() {
   this.Given(/^I should see the image of "([^"]*)" on the page$/, {timeout: process.env.StepTimeoutInMS}, function (imageName) {
-    // var imageFullPath = this.fs_session.getImageFullPath(__dirname, imageName);
-    var imageFullPath = this.fs_session.getGlobalImageFullPath(imageName);
+    var imageFullPath = this.fs_session.getLocalThenGlobalImageFullPath(__dirname, imageName);
     var imageSimilarity = 0.8;
-
-    browser.url('file://' + imageFullPath);
     var resultString = this.screen_session.focusedFindImage(imageFullPath, imageSimilarity);
     expect(resultString).not.toContain('not found');
     var resultArray = JSON.parse(resultString);
     console.log(resultArray[0].location)
-    this.screen_session.focusedRightClickImage(imageFullPath, imageSimilarity);
-    browser.pause(5000);
   });
 };
--- test-projects/Proto/Examples/features/webdriver_hub.feature
+++ test-projects/Proto/Examples/features/webdriver_hub.feature
@@ -4,9 +4,13 @@ Feature: Use browser inside steps
   I want to open browser and visit Selenium Standalone
 
   @watch
-  Scenario: Visit Webdriver Hub
+  Scenario: Visit Webdriver Hub - check page title
     When  I visit "http://localhost:4444/wd/hub"
     Then  I should see the page title to contain the keyword "webdriver hub"
-    And   I should see the image of "gLogo" on the page
+    And   I should see the image of "Selenium_CreateSession_Button" on the page
+
+
+
+    
 
     
