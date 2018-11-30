47ce15c64bc686f7e10b0c805406a65f0017bf13
xywang68 <xywang68@gmail.com>
2018-11-30 01:21:57 -0800

    should use Then

--- test-projects/Proto/Examples/features/step_definitions/Then/Then_I_should_see_the_page_title_to_contain_#.js
+++ test-projects/Proto/Examples/features/step_definitions/Then/Then_I_should_see_the_page_title_to_contain_#.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-  this.Given(/^I should see the page title to contain "([^"]*)"$/, {timeout: process.env.StepTimeoutInMS}, function (keyword) {
+  this.Then(/^I should see the page title to contain "([^"]*)"$/, {timeout: process.env.StepTimeoutInMS}, function (keyword) {
     expect(browser.getTitle().toLowerCase()).toContain(keyword);
   });
 };
