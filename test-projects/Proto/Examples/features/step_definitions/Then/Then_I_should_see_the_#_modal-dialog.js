6d50f5919155e3d7f04cec5cc9ad0c1ac5235a45
xywang68 <xywang68@gmail.com>
2018-11-30 01:21:34 -0800

    should use Then

--- test-projects/Proto/Examples/features/step_definitions/Then/Then_I_should_see_the_#_modal-dialog.js
+++ test-projects/Proto/Examples/features/step_definitions/Then/Then_I_should_see_the_#_modal-dialog.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-  this.Given(/^I should see the "([^"]*)" modal\-dialog$/, {timeout: process.env.StepTimeoutInMS}, function (modalText) {
+  this.Then(/^I should see the "([^"]*)" modal\-dialog$/, {timeout: process.env.StepTimeoutInMS}, function (modalText) {
     const seleniumPage_selectors = this.seleniumPage_selectors;
     const myModalDialog = seleniumPage_selectors.texted_modalDialog.replace('__TEXT__', modalText);
     expect(browser.isExisting(myModalDialog)).toBe(true);
