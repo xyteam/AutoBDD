34cd5c3c26217e348ca94714465261246eb55dbf
xywang68 <xywang68@gmail.com>
2018-11-30 01:21:08 -0800

    should use Then; display found image path; fail condition to include error

--- test-projects/Proto/Examples/features/step_definitions/Then/Then_I_should_see_the_#_image_on_the_page.js
+++ test-projects/Proto/Examples/features/step_definitions/Then/Then_I_should_see_the_#_image_on_the_page.js
@@ -1,9 +1,11 @@
 module.exports = function() {
-  this.Given(/^I should see the "([^"]*)" image on the page$/, {timeout: process.env.StepTimeoutInMS}, function (imageName) {
+  this.Then(/^I should see the "([^"]*)" image on the page$/, {timeout: process.env.StepTimeoutInMS}, function (imageName) {
     var imageFullPath = this.fs_session.getLocalThenGlobalImageFullPath(__dirname, imageName);
     var imageSimilarity = 0.8;
+    console.log(imageFullPath);
     var resultString = this.screen_session.focusedFindImage(imageFullPath, imageSimilarity);
     expect(resultString).not.toContain('not found');
+    expect(resultString).not.toContain('error');
     var resultArray = JSON.parse(resultString);
     console.log(resultArray[0].location)
   });
