c6dbec7a06af4a8edf03df94d0265ebdaae41d2a
xywang68 <xywang68@gmail.com>
2018-11-30 01:23:58 -0800

    should use When

--- test-projects/Proto/Examples/features/step_definitions/When/When_I_click_the_#_button_on_the_page.js
+++ test-projects/Proto/Examples/features/step_definitions/When/When_I_click_the_#_button_on_the_page.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-  this.When(/^I click the "([^"]*)" button on the page$/, function (buttonName) {
+  this.When(/^I click the "([^"]*)" button on the page$/, {timeout: process.env.StepTimeoutInMS}, function (buttonName) {
     const seleniumPage_selectors = this.seleniumPage_selectors;
     const myButton = seleniumPage_selectors.texted_button.replace('__TEXT__', buttonName);
     browser.click(myButton);
