b161ae83e7e1ea7301600144e711a36bd7290b4a
xywang68 <xywang68@gmail.com>
2018-12-02 15:33:00 -0800

    improve local control

--- test-projects/webtest-example/test-webpage/chimp.js
+++ test-projects/webtest-example/test-webpage/chimp.js
@@ -1,1 +1,3 @@
-module.exports = require('../global/support/project_chimp.js');
+require('../global/support/env.js');
+const projectFullPath = process.env.FrameworkPath + '/test-projects/' + process.env.ThisProject;
+module.exports = require(projectFullPath + '/global/support/project_chimp.js');
