d148626ffa5403e1d43b12da5669295cfcf5d8e4
xywang68 <xywang68@gmail.com>
2018-12-02 00:59:46 -0800

    rename test project

--- test-projects/webtest-example/global/support/env.js
+++ test-projects/webtest-example/global/support/env.js
@@ -1,3 +1,4 @@
+require(process.env.FrameworkPath + '/framework/support/framework_env.js');
 // define global Env vars here
-process.env.ThisProject = 'Proto';
+process.env.ThisProject = 'webtest-example';
 process.env.GlobalEnv = 'defined in glbal/env.js';
