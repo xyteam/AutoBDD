efd3818514ac288030b2037be521bd439bfddc23
xywang68 <xywang68@gmail.com>
2018-12-10 00:15:00 -0800

    add checking to detect target desktop icon

--- framework/libs/rdesktop_target.js
+++ framework/libs/rdesktop_target.js
@@ -10,12 +10,22 @@ process.env.DISPLAY = process.env.DISPLAY || ':0'
 const frameworkPath = process.env.FrameworkPath;
 require(frameworkPath + '/framework/support/framework_env.js');
 const framework_libs = require(frameworkPath + '/framework/libs/framework_libs');
+const screen_session = require(frameworkPath + '/framework/libs/screen_session');
 const execSync = require('child_process').execSync;
 
 if (start) {
   framework_libs.startSshTunnel();
   execSync('sleep 5');
-  framework_libs.startRdesktop();  
+  framework_libs.startRdesktop();
+  try {
+    var targetImagePath = frameworkPath + '/framework/support/framework_images/windows10_startButton.png';
+    var imageSimilarity = 0.8;
+    var imageWaitTime = 10;
+    screen_session.screenWaitImage(targetImagePath, imageSimilarity, imageWaitTime);
+    console.log('can see desktop');
+  } catch(e) {
+    console.log('cannot see desktop');
+  }
 }
 
 if (stop) {
