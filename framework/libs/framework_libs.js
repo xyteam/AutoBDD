7593481854298312d6cc48479c6c00f249b838df
xywang68 <xywang68@gmail.com>
2018-12-10 00:14:15 -0800

    increase RDP screen color depth

--- framework/libs/framework_libs.js
+++ framework/libs/framework_libs.js
@@ -64,7 +64,7 @@ const cmd_stop_sshfs_mount = 'if mountpoint -q ' + myDownloadPathLocal
 // rdesktop
 const cmd_check_rdesktop = 'pgrep -f "rdesktop .*' + myRDPHOST + ':' + myRDPPORT + '"';
 const cmd_start_rdesktop = 'DISPLAY=' + myDISPLAY
-                          + ' rdesktop -fa 15 -mE '
+                          + ' rdesktop -fa 16 -mE '
                           + myRDPConnString
                           + ' &';
 const cmd_stop_rdesktop = 'pkill -f "rdesktop .*' + myRDPHOST + ':' + myRDPPORT + '"';
