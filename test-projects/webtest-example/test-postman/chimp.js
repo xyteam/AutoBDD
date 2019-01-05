b3774454de38e5f39e86da108a68718593df7eaf
xywang68 <xywang68@gmail.com>
2019-01-05 03:39:58 -0800

    test-postman doesnot need platform and browser config

--- test-projects/webtest-example/test-postman/chimp.js
+++ test-projects/webtest-example/test-postman/chimp.js
@@ -1,3 +1,6 @@
 require('../global/support/env.js');
 const projectFullPath = process.env.FrameworkPath + '/test-projects/' + process.env.ThisProject;
+// API test is done on Linux platform with Chrome browser only
+process.env.PLATFORM = 'Linux';
+process.env.BROWSER = 'CH';
 module.exports = require(projectFullPath + '/global/support/project_chimp.js');
