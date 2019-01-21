10942cc318765eb1768b75c982cef4e62d4e81de
xywang68 <xywang68@gmail.com>
2019-01-20 21:23:02 -0800

    increase step timeout

--- test-projects/webtest-example/test-java/features/step_definitions/When/When_I_run_the_java_cucumber_scenario_#.js
+++ test-projects/webtest-example/test-java/features/step_definitions/When/When_I_run_the_java_cucumber_scenario_#.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-       this.When(/^I run the java cucumber scenario "([^"]*)"$/, {timeout: process.env.StepTimeoutInMS * 2}, function (javacucumberScenario) {
+       this.When(/^I run the java cucumber scenario "([^"]*)"$/, {timeout: process.env.StepTimeoutInMS * 4}, function (javacucumberScenario) {
         var result = this.javacucumber_session.runMvnTestScenario(this.javacucumber_project,
                                                                     this.javacucumber_projectModule,
                                                                     this.javacucumber_featureFile,
