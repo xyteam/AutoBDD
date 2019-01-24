cd8520645a3560621c32b6fa0f6eb6e2a6469dd9
Xinyu Wang <xinyuwang@admins-mbp.na.corp.ipgnetwork.com>
2019-01-24 12:57:15 -0800

    add test-init module

---
+++ test-projects/webtest-example/test-init/chimp.js
@@ -0,0 +1,7 @@
+require('../project/support/project_env.js');
+const projectFullPath = process.env.FrameworkPath + '/test-projects/' + process.env.ThisProject;
+
+var moduleChimp = require(projectFullPath + '/project/support/project_chimp.js');
+// modify or add myChimp attributes as necessary
+moduleChimp.offline=false;
+module.exports = moduleChimp; 
