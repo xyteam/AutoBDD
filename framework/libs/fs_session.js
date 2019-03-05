75c40d1fef7f3ab25f78144dc6839795c667037b
xwmbww <45699846+xwmbww@users.noreply.github.com>
2019-03-05 11:40:19 -0800

    unified testfile and testimage search order to module -> project -> framework (#31)

--- framework/libs/fs_session.js
+++ framework/libs/fs_session.js
@@ -1,8 +1,10 @@
 // fs_session.js provides functions to read and write the Downlaods folder of the target system (local or remote)
 const FrameworkPath = process.env.FrameworkPath || process.env.HOME + '/Projects/AutoBDD';
-const FrameworkImagePath = FrameworkPath + 'framework/support/images';
+const FrameworkSupportPath = FrameworkPath + '/framework/support';
 const ProjectPath = FrameworkPath + '/test-projects/' + process.env.ThisProject;
-const ProjectImagePath = ProjectPath + '/project/support/images'
+const ProjectSupportPath = ProjectPath + '/project/support'
+const ModulePath = ProjectPath + '/' + process.env.ThisModule;
+const ModuleSupportPath = ModulePath + '/support';
 const DownloadPathLocal = process.env.DownloadPathLocal;
 const fs = require('fs');
 const pdfParse = require('pdf-parse');
@@ -10,110 +12,122 @@ const XLSX = require('xlsx');
 const execSync = require('child_process').execSync;
 
 module.exports = {
-  getTestFileFullPath: function(filePath, fileName, fileExt) {
+  getTestFileFullPath: function(fileName, fileExt) {
     var testFileExt = fileExt || ['json'];
-    var targetPath = filePath;
     var testFileFullPath = null;
 
-    testFileExt.some(function(ext) {
-      var fileFullPath = targetPath + '/' + fileName + '.' + ext;
-      if (fs.existsSync(fileFullPath)) testFileFullPath = fileFullPath;
-    })
+    // Search order: Module, Project, Support, will return null if not found
+    if (testFileFullPath == null && process.env.ThisProject && process.env.ThisModule) {
+      testFileExt.some(function(ext) {
+        var filePath = ModuleSupportPath + '/testfiles/' + fileName + '.' + ext;
+        if (fs.existsSync(filePath)) testFileFullPath = filePath;
+      })
+    }
+    if (testFileFullPath == null && process.env.ThisProject) {
+      testFileExt.some(function(ext) {
+        var filePath = ProjectSupportPath + '/testfiles/' + fileName + '.' + ext;
+        if (fs.existsSync(filePath)) testFileFullPath = filePath;
+      })
+    }
+    if (testFileFullPath == null && process.env.FrameworkPath) {
+      testFileExt.some(function(ext) {
+        var filePath = FrameworkSupportPath + '/testfiles/' + fileName + '.' + ext;
+        if (fs.existsSync(filePath)) testFileFullPath = filePath;
+      })
+    }
     return testFileFullPath;
   },
 
-  getLocalTestFileFullPath: function(filePath, fileName, fileExt) {
-    var targetPath = filePath.substring(0, filePath.indexOf('steps')) + 'files';
-    var testFileFullPath = this.getTestFileFullPath(targetPath, fileName, fileExt);
-    return testFileFullPath;
-  },
-
-  getTestImageFullPath: function(filePath, fileName, fileExt) {
+  getTestImagePath: function(filePath, fileName, fileExt) {
     var imageExt = fileExt || ['gif', 'jpg', 'png'];
     var targetPath = filePath;
     var platformBrowserXVFBPath = '/' + process.env.PLATFORM + '/' + process.env.BROWSER + '/' + process.env.XVFB;
     var platformXVFBPath = '/' + process.env.PLATFORM + '/' + process.env.XVFB;
     var platformBrowserPath = '/' + process.env.PLATFORM + '/' + process.env.BROWSER;
     var platformOnlyPath = '/' + process.env.PLATFORM;
-    var imageFullPath = null;
+    var imagePath = null;
 
     // TESTIMAGE/PLATFORM/BROWSER/XVFB/
-    if ((imageFullPath == null) && (process.env.XVFB == 'XVFB') && fs.existsSync(targetPath + platformBrowserXVFBPath)) {
+    if ((imagePath == null) && (process.env.XVFB == 'XVFB') && fs.existsSync(targetPath + platformBrowserXVFBPath)) {
       imageExt.some(function(ext) {
-        var fileFullPath = targetPath + platformBrowserXVFBPath + '/' + fileName + '.' + ext;
-        if (fs.existsSync(fileFullPath)) imageFullPath = fileFullPath;
+        var filePath = targetPath + platformBrowserXVFBPath + '/' + fileName + '.' + ext;
+        if (fs.existsSync(filePath)) imagePath = filePath;
       });
     }
     // TESTIMAGE/PLATFORM/BROWSER/
-    if ((imageFullPath == null) && fs.existsSync(targetPath + platformBrowserPath)) {
+    if ((imagePath == null) && fs.existsSync(targetPath + platformBrowserPath)) {
       imageExt.some(function(ext) {
-        var fileFullPath = targetPath + platformBrowserPath + '/' + fileName + '.' + ext;
-        if (fs.existsSync(fileFullPath)) imageFullPath = fileFullPath;
+        var filePath = targetPath + platformBrowserPath + '/' + fileName + '.' + ext;
+        if (fs.existsSync(filePath)) imagePath = filePath;
       });
     }
     // TESTIMAGE/PLATFORM/XVFB/
-    if ((imageFullPath == null) && (process.env.XVFB == 'XVFB') && fs.existsSync(targetPath + platformXVFBPath)) {
+    if ((imagePath == null) && (process.env.XVFB == 'XVFB') && fs.existsSync(targetPath + platformXVFBPath)) {
       imageExt.some(function(ext) {
-        var fileFullPath = targetPath + platformXVFBPath + '/' + fileName + '.' + ext;
-        if (fs.existsSync(fileFullPath)) imageFullPath = fileFullPath;
+        var filePath = targetPath + platformXVFBPath + '/' + fileName + '.' + ext;
+        if (fs.existsSync(filePath)) imagePath = filePath;
       });
     }
     // TESTIMAGE/PLATFORM/
-    if ((imageFullPath == null) && fs.existsSync(targetPath + platformOnlyPath)) {
+    if ((imagePath == null) && fs.existsSync(targetPath + platformOnlyPath)) {
       imageExt.some(function(ext) {
-        var fileFullPath = targetPath + platformOnlyPath + '/' + fileName + '.' + ext;
-        if (fs.existsSync(fileFullPath)) imageFullPath = fileFullPath;
+        var filePath = targetPath + platformOnlyPath + '/' + fileName + '.' + ext;
+        if (fs.existsSync(filePath)) imagePath = filePath;
       });
     }
     // TESTIMAGE/
-    if ((imageFullPath == null) && fs.existsSync(targetPath)) {
+    if ((imagePath == null) && fs.existsSync(targetPath)) {
       imageExt.some(function(ext) {
-        var fileFullPath = targetPath + '/' + fileName + '.' + ext;
-        if (fs.existsSync(fileFullPath)) imageFullPath = fileFullPath;
+        var filePath = targetPath + '/' + fileName + '.' + ext;
+        if (fs.existsSync(filePath)) imagePath = filePath;
       });
     }
-    return imageFullPath;
+    return imagePath;
   },
 
-  getLocalImageFullPath: function(filePath, fileName, fileExt) {
-    var targetPath = filePath.substring(0, filePath.indexOf('steps')) + 'images';
-    var imageFullPath = this.getTestImageFullPath(targetPath, fileName, fileExt);
-    return imageFullPath;
+  getModuleImagePath: function(filePath, fileName, fileExt) {
+    var targetPath = ModuleSupportPath + '/testimages/';
+    var imagePath = this.getTestImagePath(targetPath, fileName, fileExt);
+    return imagePath;
   },
 
-  getGlobalImageFullPath: function(fileName, fileExt) {
-    var targetPath = ProjectImagePath;
-    var imageFullPath = this.getTestImageFullPath(targetPath, fileName, fileExt);
-    return imageFullPath;
+  getProjectImagePath: function(fileName, fileExt) {
+    var targetPath = ProjectSupportPath + '/testimages/';
+    var imagePath = this.getTestImagePath(targetPath, fileName, fileExt);
+    return imagePath;
   },
 
-  getFrameworkImageFullPath: function(fileName, fileExt) {
-    var targetPath = FrameworkImagePath;
-    var imageFullPath = this.getTestImageFullPath(targetPath, fileName, fileExt);
-    return imageFullPath;
+  getFrameworkImagePath: function(fileName, fileExt) {
+    var targetPath = FrameworkSupportPath + '/testimages/';
+    var imagePath = this.getTestImagePath(targetPath, fileName, fileExt);
+    return imagePath;
   },  
 
-  getLocalThenGlobalImageFullPath: function(filePath, fileName, fileExt) {
-    var imageFullPath = this.getLocalImageFullPath(filePath, fileName, fileExt);
-    if (!imageFullPath) {
-      imageFullPath = this.getGlobalImageFullPath(fileName, fileExt);
+  globalSearchImagePath: function(filePath, fileName, fileExt) {
+    // Search order: Module, Project, Support, will return null if not found
+    var imagePath = this.getModuleImagePath(filePath, fileName, fileExt);
+    if (!imagePath) {
+      imagePath = this.getProjectImagePath(fileName, fileExt);
     }
-    return imageFullPath;
+    if (!imagePath) {
+      imagePath = this.getFrameworkImagePath(fileName, fileExt);
+    }
+    return imagePath;
   },
 
   deleteDownloadFile: function(fileName, fileExt) {
-    const fileFullPath = DownloadPathLocal + '/' + fileName + '.' + fileExt;
+    const filePath = DownloadPathLocal + '/' + fileName + '.' + fileExt;
     const oldFilesFullPath = DownloadPathLocal + '/' + fileName + '\\ \\([0-9]*\\)' + '.' + fileExt;
-    const rm_downloadFile_cmd = 'rm -f ' + fileFullPath + ' ' + oldFilesFullPath;
+    const rm_downloadFile_cmd = 'rm -f ' + filePath + ' ' + oldFilesFullPath;
     execSync(rm_downloadFile_cmd);
   },
 
   checkDownloadFile: function(fileName, fileExt) {
-    const fileFullPath = DownloadPathLocal + '/' + fileName + '.' + fileExt;
-    while (!fs.existsSync(fileFullPath)) {
+    const filePath = DownloadPathLocal + '/' + fileName + '.' + fileExt;
+    while (!fs.existsSync(filePath)) {
       browser.pause(1000);
     }
-    return fileFullPath;
+    return filePath;
   },
 
   readPdfData: function(pdfFullPath) {
