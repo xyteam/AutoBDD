2b4905426e1ccd7f7cf14c32a300e7e350ebd88b
xwmbww <45699846+xwmbww@users.noreply.github.com>
2019-03-13 12:04:16 -0700

    Xw auto project module path (#40)
    * added dropzone case
    
    * auto-detect project and module names
    
    * Revert "added dropzone case"
    
    This reverts commit 7375850878af3383f3dd912de5c4ffd60d0f4b66.

--- test-projects/webtest-example/test-init/support/env.js
+++ test-projects/webtest-example/test-init/support/env.js
@@ -1,4 +1,10 @@
 require('../../project/support/env.js');
-process.env.ThisModule = 'test-init';
+// determine module name and path
+var moduleName = __dirname.substring(0, __dirname.lastIndexOf('/support'));
+moduleName = moduleName.substring(moduleName.lastIndexOf('/') + 1);
+process.env.ThisModule = moduleName;
 process.env.ModulePath = process.env.ProjectPath + '/' + process.env.ThisModule;
+// console.log(process.env.ThisModule);
+// console.log(process.env.ModulePath);
+
 // define module level Env vars here
