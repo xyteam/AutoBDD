5be8a1d6d729e43fc1c2dc16db209515819b795f
Hendry <hendry.ang@mbww.com>
2019-03-25 01:35:33 +0800

    handle update, extract steps, sprint ID

--- framework/libs/testrail_libs.js
+++ framework/libs/testrail_libs.js
@@ -1,5 +1,6 @@
 
 const Testrail = require('testrail-api');
+const _ = require('underscore');
 const testrail = new Testrail({
   host: process.env.trApiUrl,
   user: process.env.trApiUser,
@@ -48,6 +49,61 @@ module.exports = {
         return mySection.id;
     } catch (e) {
     }
-  }
+  },
 
+  extractSteps: function (feature , type , scenarioName = ""){
+    //to extract all the steps (including tables) from a given feature and type (background or scenario)
+    try {
+      scenarioName = (type == 'background') ? "" : scenarioName;
+      var stepBuilder = "";
+      if ( !_.isEmpty ( feature ) ) {
+          var targetScenario = feature.elements.filter ( s => (s.type === type && s.name === scenarioName));
+          if (targetScenario.length >= 1) { //assume single unique scenario (todo: handle outline?)
+              targetScenario[0].steps.filter ( s => _.contains(["given","when","then","but","and"], s.keyword.trim().toLowerCase())).forEach ( step => {
+                stepBuilder += step.keyword + ' ' + step.name + '\r\n';
+                if ( _.has (step , "rows" )){
+                    step.rows.forEach (row => {
+                        row.cells.forEach ( cell => {
+                            stepBuilder += '|' + cell ;
+                        });
+                        stepBuilder += '|\r\n';
+                    })
+                }
+              });
+          } else {
+              console.log ("Unable to find type of \"" + type + "\" in provided feature file");
+          }
+      } else {
+          console.log ("Provided feature is empty");
+      }    
+      return stepBuilder;
+    } catch (e) {      
+    }    
+  },
+
+  getFeature_ByScenario: function ( scenarioName , jsonData) {
+    //to get single feature object that contains the given scenarioName (assume unique scenario)
+    var targetFeature = {};
+    jsonData.forEach ( feature => {
+        feature.elements.forEach ( scenario => {
+            if ( scenario.name === scenarioName ) {        
+              targetFeature = feature;                                
+            }
+        })
+    })
+    return targetFeature;    
+  },
+
+  getCurrentSprintID : function (){
+    //get current sprint number, used to handle milestone/runs naming.
+    const TZ = "+8";
+    const MS_TO_DAY = 86400000; //milliseconds to day
+    const START_SPRINT = 100;
+    const SPRINT_LENGTH = 14;
+    const START_DATE = "March 04, 2019 00:00:00";
+    var epochDate = date => Date.parse ( new Date ( date.getTime() + (date.getTimezoneOffset() * 60000) + (3600000 * TZ)))
+    curDate = epochDate(new Date());
+    startDate = epochDate (new Date(START_DATE));
+    return ( Math.floor(((curDate - startDate)/MS_TO_DAY/SPRINT_LENGTH) + START_SPRINT ));
+  }
 }
