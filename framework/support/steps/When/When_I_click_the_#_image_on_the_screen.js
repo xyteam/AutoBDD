3a5c783ffcf592b899a682a301304dd1908323f5
Xinyu Wang <xwang@VSN00023.local>
2019-05-03 14:15:02 -0700

    update chrome driver version; update minor screen_session; update test example step to use new screen_session function

--- framework/support/steps/When/When_I_click_the_#_image_on_the_screen.js
+++ framework/support/steps/When/When_I_click_the_#_image_on_the_screen.js
@@ -1,10 +1,18 @@
 module.exports = function() {
-  this.When(/^I click the "([^"]*)" image on the screen$/, {timeout: process.env.StepTimeoutInMS}, function (imageName) {
+  this.When(/^I (click|hover) the "([^"]*)" image on the screen$/, {timeout: process.env.StepTimeoutInMS}, function (mouseAction, imageName) {
     var imageFullPath = this.fs_session.globalSearchImagePath(__dirname, imageName);
     var imageSimilarity = process.env.imageSimilarity;
     var imageWaitTime = process.env.imageWaitTime;
     console.log(imageFullPath);
-    var resultString = this.screen_session.screenFindImage(imageFullPath, imageSimilarity, imageWaitTime, 'single');
+    switch (mouseAction) {
+      case "hover":
+      break;
+      case "click":
+      resultString = this.screen_session.screenHoverImage(imageFullPath, imageSimilarity, imageWaitTime);
+      default:
+      break;
+    }
+    var resultString = this.screen_session.screenClickImage(imageFullPath, imageSimilarity, imageWaitTime);
     expect(resultString).not.toContain('not found');
     expect(resultString).not.toContain('error');
     var resultArray = JSON.parse(resultString);
