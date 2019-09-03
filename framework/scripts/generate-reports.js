467f465b4e7d0d32e60c772b3f97e7d260480cbd
xywang68 <xywang68@gmail.com>
2019-09-03 11:43:38 -0700

    update for 1.0.4

--- framework/scripts/generate-reports.js
+++ framework/scripts/generate-reports.js
@@ -1,49 +1,188 @@
 #!/usr/bin/env node
 
-var argv = require('minimist')(process.argv.slice(2));
+const meow = require('meow');
 const fs = require('fs');
-const reporter = require('cucumber-html-reporter');
+const bootStrapHtmlReporter = require('cucumber-html-reporter');
+const searchableHtmlReporter = require('multiple-cucumber-html-reporter');
 
-var report_on = process.argv[2]; 
-var report_name = process.argv[3] || 'Project';
-var report_brandTitle = process.argv[4] || 'Automation Report';
-var report_platform = process.argv[5] || 'Linux';
-var report_browser = process.argv[6] || 'CH';
-var report_threads = process.argv[7] || '';
-var report_startTime = process.argv[8] || '';
-var report_runDuration = process.argv[9] || '';
-var report_rerunPath = process.argv[10] || '';
-var report_args = process.argv[11] || '';
+optionHelp = `
+    Usage
+        $ generate-reports.js --optionName1=optionValue1, --optionName2=optionValue2 ...
+    Options
+        --reportType, there are 2 basic types: cucumber and searchable which can be combined.
+            cucumber has 4 sub-types: cucumber-bootstrap, cucumber-hierarchy, cucumber-foundation and cucumber-simple.
+        --reportJson, thejsonFile that contains the test result.
+        --reportName, master name of the report, such as the name of the product or project.
+        --reportTitle, purpose of the test, such as E2E Test, Demo Test.
+        --testPlatform, the test platform
+        --testPlatformVer, the test platform version
+        --testDevice, the device this reported test is run on
+        --testBrowser, the browser this reported test is run on
+        --testBrowserVer, the browser version this reported test is run on
+        --testThreads, the parallel test threads
+        --testStartTime, the start timestamp
+        --testRunDuration, how long it took to run the entire test
+        --testRerunPath, if this is a re-run which report path it re-run against
+        --testRunArgs, the run arguments used for running the test, i.e., --modulelist moduleA module B, --tags @SmokeTest, etc. 
+`;
+optionFlags = {
+    reportType: {
+        type: 'string',
+        default: 'cucumber, searchable'
+    },
+    reportJson: {
+        type: 'string',
+        default: './cucumber-report.json'
+    },
+    reportName: {
+        type: 'string',
+        default: 'AutoBDD'
+    },
+    reportTitle: {
+        type: 'string',
+        default: 'AutoBDD Test Report'
+    },
+    testPlatform: {
+        type: 'string',
+        default: 'Linux'
+    },
+    testPlatformVer: {
+        type: 'string',
+        default: 'Ubuntu 18.04'
+    },
+    testDevice: {
+        type: 'string',
+        default: 'NA'
+    },
+    testBrowser: {
+        type: 'string',
+        default: 'Chrome'
+    },
+    testBrowserVer: {
+        type: 'string',
+        default: '76'
+    },
+    testThreads: {
+        type: 'string'
+    },
+    testStartTime: {
+        type: 'string'
+    },
+    testRunDuration: {
+        type: 'string'
+    },
+    testRerunPath: {
+        type: 'string'
+    },
+    testRunArgs: {
+        type: 'string'
+    }
+}
 
-var options = {
+const cli = meow(optionHelp, {flags: optionFlags});
+var reportType = cli.flags.reportType;
+var reportJson = cli.flags.reportJson;
+var reportName = cli.flags.reportName;
+var reportTitle = cli.flags.reportTitle;
+var testPlatform = cli.flags.testPlatform;
+var testPlatformVer = cli.flags.testPlatformVer;
+var testDevice = cli.flags.testDevice;
+var testBrowser = cli.flags.testBrowser;
+var testBrowserVer = cli.flags.testBrowserVer;
+var testThreads = cli.flags.testThreads;
+var testStartTime = cli.flags.testStartTime;
+var testRunDuration = cli.flags.testRunDuration;
+var testRerunPath = cli.flags.testRerunPath;
+var testRunArgs = cli.flags.testRunArgs;
+
+var bootStrapHtmlReporter_options = {
         launchReport: false,
-        ignoreBadJsonFile: true,
+        ignoreBadJsonFile: false,
         reportSuiteAsScenarios: false,
         metadata: {
-            "Project": report_name,
-            "Platform": report_platform,
-            "Browser": report_browser,
-            "Run Threads": report_threads,
-            "Start Time": report_startTime,
-            "Run Duration": report_runDuration,
-            "Rerun Path": report_rerunPath,
-            "Run Args": report_args
+            "Project": reportName,
+            "Title": reportTitle,
+            "Platform": testPlatform,
+            "Device": testDevice,
+            "Browser": testBrowser,
+            "browserVersion": testBrowserVer,
+            "Run Threads": testThreads,
+            "Start Time": testStartTime,
+            "Run Duration": testRunDuration,
+            "Rerun Path": testRerunPath,
+            "Run Args": testRunArgs
         }
     };
 
-if (fs.lstatSync(report_on).isDirectory()) {
-    options.jsonDir = report_on;
-    options.theme = 'foundation';
-    options.output = report_on + '/cucumber-report.html'
-} else if (fs.lstatSync(report_on).isFile()) {
-    options.jsonFile = report_on;
-    options.theme = 'bootstrap';
-    options.output = report_on + '.html';
+    var searchableHtmlReporter_options = {
+        metadata: {
+            browser: {
+                name: testBrowser,
+                version: testBrowserVer
+            },
+            device: testDevice,
+            platform: {
+                name: testPlatform,
+                version: testPlatformVer
+            },
+        },
+        customData: {
+            title: reportTitle,
+            data: [
+                {label: 'Run Threads', value: testThreads},
+                {label: 'Start Time', value: testStartTime},
+                {label: 'Run Duration', value: testRunDuration},
+                {label: 'Run Args', value: testRunArgs},
+            ]
+        }    
+    };
+
+if (fs.lstatSync(reportJson).isFile()) {
+    if (reportType.includes('cucumber')) {
+        //generate report preparation
+        bootStrapHtmlReporter_options.jsonFile = reportJson;
+        switch (true) {
+            case reportType.includes('bootstrap'):
+                bootStrapHtmlReporter_options.theme = 'bootstrap';
+                break;
+            case reportType.includes('hierarchy'):
+                bootStrapHtmlReporter_options.theme = 'hierarchy';
+                break;
+            case reportType.includes('foundation'):
+                bootStrapHtmlReporter_options.theme = 'foundation';
+                break;
+            case reportType.includes('simple'):
+                bootStrapHtmlReporter_options.theme = 'simple';
+                break;
+            default:
+                bootStrapHtmlReporter_options.theme = 'bootstrap';
+                break;
+        }
+        bootStrapHtmlReporter_options.output = reportJson.substring(0, reportJson.lastIndexOf('.json')) + '.html';
+        bootStrapHtmlReporter_options.name = reportName;
+        bootStrapHtmlReporter_options.brandTitle = reportTitle;
+        // generate report action
+        bootStrapHtmlReporter.generate(bootStrapHtmlReporter_options);
+    }
+    if (reportType.includes('searchable')) {
+        // generate searchable theme report
+        searchableHtmlReporter_options.jsonDir = reportJson.match(/.*\//)[0];
+        searchableHtmlReporter_options.reportPath = searchableHtmlReporter_options.jsonDir;
+        searchableHtmlReporter_options.displayDuration = true;
+
+        searchableHtmlReporter.generate(searchableHtmlReporter_options);
+        // create symbolic link to screenshot and movie
+        var itemList = fs.readdirSync(searchableHtmlReporter_options.jsonDir);
+        var dirList = itemList.filter(n => n != 'assets' && n!= 'features' && fs.lstatSync(searchableHtmlReporter_options.jsonDir + '/' + n).isDirectory())
+        dirList.forEach(dirName => {
+            try {
+                fs.symlinkSync('../' + dirName, searchableHtmlReporter_options.jsonDir + '/features/' + dirName)
+            } catch (e) {
+                console.log('cannot create symlink for feature reports: ' + dirName)
+            }
+        })
+    }
 } else {
-    console.log('Reporter Error: 1st parameter need to be a jsonDir or jsonFile.');
+    console.log('Reporter Error: jsonFile need to a cucumber jsonFile.');
     process.exit(1);
 }
-
-options.name = report_name;
-options.brandTitle = report_brandTitle;
-reporter.generate(options);
