60a4d21c682d114be707f014f92b5f5d458a5bff
xywang68 <xywang68@gmail.com>
2019-09-11 19:57:33 -0700

    Add test steps

--- framework/support/steps/Then/Then_the_downloaded_XLS_file_should_contain_X_rows.js
+++ framework/support/steps/Then/Then_the_downloaded_XLS_file_should_contain_X_rows.js
@@ -1,12 +1,19 @@
 module.exports = function() {
-  this.Then(/^the downloaded XLS file should contain (\d+) rows$/, {timeout: process.env.StepTimeoutInMS}, function (rowCount) {
+  this.Then(/^the downloaded XLS file should contain (\d+) rows and (\d+) columns$/, {timeout: process.env.StepTimeoutInMS}, function (rowCount, columnCoung) {
     // get downloaded filename
     var downloadUrl = this.downloadUrl;
     console.log(downloadUrl);
     var fileName = downloadUrl.substring(downloadUrl.lastIndexOf('/') + 1, downloadUrl.lastIndexOf('.'));
     var fileExt = downloadUrl.substring(downloadUrl.lastIndexOf('.') + 1);
     var downloadFilePath = this.fs_session.checkDownloadFile(fileName, fileExt);
-    var xlsData = this.fs_session.readXlsData(downloadFilePath);
-    expect(xlsData.length).toEqual(parseInt(rowCount));
+    var xlsData = this.fs_session.readXlsData(downloadFilePath).filter(row => row.length > 0);
+    if (rowCount) {
+      expect(xlsData.length).toEqual(parseInt(rowCount));
+    }
+    if (columnCoung) {
+      expect(xlsData[0].length).toEqual(parseInt(columnCoung));
+    }
+    // pass download file path to subsequent steps
+    this.downloadFilePath = downloadFilePath;
   });
 };
