3609505823b1f04485a5e3a795e3b30438301987
xwvArmour <50221398+xwvArmour@users.noreply.github.com>
2019-10-02 14:12:29 -0700

    Update autobdd-run.startup.sh (#96)

--- docker/autobdd-run.startup.sh
+++ docker/autobdd-run.startup.sh
@@ -3,21 +3,30 @@
 USER=${USER:-root}
 HOME=/root
 if [ "$USER" != "root" ]; then
-  echo "* enable custom user: $USER"
-  useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
-  if [ -z "$PASSWORD" ]; then
-    echo "  set default password to \"ubuntu\""
-    PASSWORD=ubuntu
-  fi
-  export HOME=/home/$USER
-  echo "$USER:$PASSWORD" | chpasswd
-  unset PASSWORD
+    echo "* enable custom user: $USER"
+    if [ "$HOSTOS" == "Linux" ]; then
+        useradd --uid $USERID --gid $GROUPID --create-home --shell /bin/bash --groups adm,sudo $USER
+    else
+        useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+    fi
+    if [ -z "$PASSWORD" ]; then
+        echo "  set default password to \"ubuntu\""
+        PASSWORD=ubuntu
+    fi
+    export HOME=/home/$USER
+    echo "$USER:$PASSWORD" | chpasswd
+    cp -r /root/{.gtkrc-2.0,.asoundrc} ${HOME}
+    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+    unset PASSWORD
 fi
 
 # home folder
 cd /root && tar cf - ./Projects | (cd $HOME && tar xf -)
-chown -R $USER:$USER $HOME
-
+if [ "$HOSTOS" == "Linux" ]; then
+  chown -R $USERID:$GROUPID $HOME
+else
+  chown -R $USER:$USER $HOME
+fi
 # set bash_profile
 cat /root/.bashrc >> $HOME/.bash_profile && chown $USER:$USER $HOME/.bash_profile
 cat >> $HOME/.bash_profile << END_bash_profile
