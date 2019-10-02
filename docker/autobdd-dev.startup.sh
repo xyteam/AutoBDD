71baa4ad4b17c2edc60b63b3367d4544161f939c
xwvArmour <50221398+xwvArmour@users.noreply.github.com>
2019-10-02 14:06:18 -0700

    Update autobdd-dev.startup.sh

--- docker/autobdd-dev.startup.sh
+++ docker/autobdd-dev.startup.sh
@@ -24,7 +24,11 @@ USER=${USER:-root}
 HOME=/root
 if [ "$USER" != "root" ]; then
     echo "* enable custom user: $USER"
-    useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+    if [ "$HOSTOS" == "Linux" ]; then
+        useradd --uid $USERID --gid $GROUPID --create-home --shell /bin/bash --groups adm,sudo $USER
+    else
+        useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+    fi
     if [ -z "$PASSWORD" ]; then
         echo "  set default password to \"ubuntu\""
         PASSWORD=ubuntu
@@ -44,14 +48,19 @@ mkdir -p /run/sshd
 # home folder
 mkdir -p $HOME/.config/pcmanfm/LXDE/
 ln -sf /usr/local/share/doro-lxde-wallpapers/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/
-chown -R $USER:$USER $HOME
+cd /root && tar cf - ./Projects | (cd $HOME && tar xf -)
+if [ "$HOSTOS" == "Linux" ]; then
+  chown -R $USERID:$GROUPID $HOME
+else
+  chown -R $USER:$USER $HOME
+fi
 
 # set bash_profile
 cat /root/.bashrc >> $HOME/.bash_profile && chown $USER:$USER $HOME/.bash_profile
 cat >> $HOME/.bash_profile << END_bash_profile
-npm config set script-shell /bin/bash
 export DISPLAY=:1
-cd $HOME/Projects/AutoBDD && \
+npm config set script-shell /bin/bash
+cd \\$HOME/Projects/AutoBDD
 source .autoPathrc.sh
 END_bash_profile
 
