0770de821723ca84259abaf533e83145e1ed15da
Xinyu Wang <xwang@varmour.com>
2019-10-22 14:58:20 -0700

    fix typo

--- framework/functions/check/checkIfElementExistsInsideParentElement.js
+++ framework/functions/check/checkIfElementExistsInsideParentElement.js
@@ -1,12 +1,12 @@
 /**
  * Check if the given element exists in the DOM one or more times
  * @param  {String}  targetElement  Element selector
- * @param  {Boolean} falsCase Check if the element (does not) exists
+ * @param  {Boolean} falseCase      Check if the element (does not) exists
  * @param  {String}  parentElement  Element selector
- * @param  {String}  exactly  Check if the element exists exactly this number (as string)
- *                            of times
+ * @param  {String}  exactly        Check if the element exists exactly this number (as string)
+ *                                  of times
  */
-module.exports = (targetElement, falsCase, parentElement, exactly) => {
+module.exports = (targetElement, falseCase, parentElement, exactly) => {
     const parentElementId = browser.element(parentElement).value.ELEMENT;
     /**
      * The number of elements found in the DOM
@@ -14,7 +14,7 @@ module.exports = (targetElement, falsCase, parentElement, exactly) => {
      */
     const nrOfElements = browser.elementIdElements(parentElementId, targetElement).value;
 
-    if (falsCase === true) {
+    if (falseCase === true) {
         expect(nrOfElements.length).toBe(
             0,
             `Element with selector "${targetElement}" should not exist inside "${parentElement}"`
