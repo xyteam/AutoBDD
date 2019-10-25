4a28adbfe44136477c59e67187920c9084b66599
Xinyu Wang <xwang@varmour.com>
2019-10-25 16:54:49 -0700

    update FF driver version

--- framework/configs/selenium-standalone_config.js
+++ framework/configs/selenium-standalone_config.js
@@ -15,7 +15,7 @@ module.exports = {
       baseURL: 'https://selenium-release.storage.googleapis.com'
     },
     firefox: {
-      version: '0.24.0',
+      version: '0.26.0',
       arch: process.arch,
       baseURL: 'https://github.com/mozilla/geckodriver/releases/download'
     },
