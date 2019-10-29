ee01b8c480b0f3bf9d458079e83f471dd3f42a6a
Xinyu Wang <xwang@varmour.com>
2019-10-28 19:31:26 -0700

    improvement refactor

--- framework/functions/action/openTarget.js
+++ framework/functions/action/openTarget.js
@@ -1,33 +1,37 @@
 /**
  * Open the given URL
  * @param  {String}   targetType targetType of navigation (url, file, site or download file)
- * @param  {String}   target The URL to navigate to
+ * @param  {String}   targetName The URL to navigate to
  */
 const globSync = require("glob").sync;
 const getDownloadDir = require('../common/getDownloadDir');
 
-module.exports = (targetType, target) => {
-    /**
-     * The URL to navigate to
-     * @targetType {String}
-     */
+module.exports = (targetType, targetName) => {
+    var fileTarget;
+    var urlTarget
     switch (targetType) {
         case "download file":
-            var myFilePath = globSync(getDownloadDir() + '/' + target)[0]; // we take the first match
-            browser.pause(2000);
-            browser.url('file://' + myFilePath);
-            browser.pause(3000);
+            fileTarget = getDownloadDir() + targetName.replace(/ /g, '\\ ');
+            urlTarget = encodeURI('file://' + globSync(fileTarget)[0]); // we take the first match
+            console.log(urlTarget)
+            browser.url(urlTarget);
             break;
         case "file":
-            var myFilePath = globSync(target)[0]; // we take the first match
-            browser.url('file://' + myFilePath);
+            fileTarget = targetName.replace(/ /g, '\ ');
+            urlTarget = encodeURI('file://' + globSync(myTarget)[0]); // we take the first match
+            console.log(urlTarget)
+            browser.url(urlTarget);
             break;
         case "path":
-            browser.url(browser.options.baseUrl + target);
+            urlTarget = encodeURI(browser.options.baseUrl + targetName);
+            console.log(urlTarget)
+            browser.url(urlTarget);
             break;
         case "url":
         default:
-            browser.url(target);
-    }
+            urlTarget = encodeURI(targetName);
+            console.log(urlTarget)
+            browser.url(urlTarget);
+        }
 };
 
