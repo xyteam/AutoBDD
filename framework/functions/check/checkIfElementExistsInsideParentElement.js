06c28a047bab54aa46da695da12600d28209901f
Xinyu Wang <xwang@varmour.com>
2019-11-08 15:08:08 -0800

    consolidated

--- framework/functions/check/checkIfElementExistsInsideParentElement.js
+++ framework/functions/check/checkIfElementExistsInsideParentElement.js
@@ -1,15 +1,17 @@
 /**
  * Check if the given element exists in the DOM one or more times
  * @param  {String}  targetElement      target element selector
- * @param  {Boolean} falseCase          Check if the element (does not) exists
  * @param  {String}  parentElementIndex The nth parent element start from 1st,2nd,3rd,4th
  * @param  {String}  parentElement      parent element selector
+ * @param  {Boolean} falseCase          Check if the element (does not) exists
  * @param  {String}  compareAction      compare action: more than, less than, exactly
  * @param  {String}  expectedNumber     Check if the element exists this number (as string) of times
  */
-module.exports = (targetElement, parentElementIndex, parentElement, falseCase, compareAction, expectedNumber) => {
+
+ module.exports = (targetElement, parentElementIndex, parentElement, falseCase, compareAction, expectedNumber) => {
     const parentElementIndexInt = (parentElementIndex) ? parseInt(parentElementIndex) - 1 : 0;
     const myExpectedNumber = (expectedNumber) ? parseInt(expectedNumber) : 0;
+    const myCompareAction = (compareAction) ? compareAction : 'more than';
 
     var appearanceNumber;
     if (parentElement) {
@@ -19,30 +21,27 @@ module.exports = (targetElement, parentElementIndex, parentElement, falseCase, c
         appearanceNumber = browser.elements(targetElement).value.length;
     }
 
-    switch (compareAction) {
+    switch (myCompareAction) {
         case 'exactly':
-            if (falseCase) {
-                expect(appearanceNumber).not.toEqual(parseInt(myExpectedNumber));
-            }
-            else {
-                expect(appearanceNumber).toEqual(parseInt(myExpectedNumber))
-            }
+            expect(appearanceNumber).toEqual(parseInt(myExpectedNumber))
+            break;
+        case 'not exactly':
+            expect(typeof falseCase === 'undefined').toBe(true, 'cannot use double negative expression');
+            expect(appearanceNumber).not.toEqual(parseInt(myExpectedNumber))
+            break;
+        case 'more than':
+            expect(appearanceNumber).toBeGreaterThan(parseInt(myExpectedNumber))
+            break;
+        case 'no more than':
+            expect(typeof falseCase === 'undefined').toBe(true, 'cannot use double negative expression');
+            expect(appearanceNumber).not.toBeGreaterThan(parseInt(myExpectedNumber))
             break;
         case 'less than':
-            if (falseCase) {
-                expect(appearanceNumber).not.toBeLessThan(parseInt(myExpectedNumber));
-            }
-            else {
-                expect(appearanceNumber).toBeLessThan(parseInt(myExpectedNumber))
-            }
+            expect(appearanceNumber).toBeLessThan(parseInt(myExpectedNumber))
             break;
-        default:
-        case 'more than':
-            if (falseCase) {
-                expect(appearanceNumber).not.toBeGreaterThan(parseInt(myExpectedNumber));
-            }
-            else {
-                expect(appearanceNumber).toBeGreaterThan(parseInt(myExpectedNumber))
-            }
+        case 'no less than':
+            expect(typeof falseCase === 'undefined').toBe(true, 'cannot use double negative expression');
+            expect(appearanceNumber).not.toBeLessThan(parseInt(myExpectedNumber))
+            break;
     }    
 };
