c6fb8b952d24845101f24e477c9a8429c5a17edc
xywang68 <xywang68@gmail.com>
2020-01-17 17:07:14 -0800

    typo update

--- test-projects/simplest-test/test-report/chimp.js
+++ test-projects/simplest-test/test-report/chimp.js
@@ -1,4 +1,4 @@
-require('./support/env.js.js')
+require('./support/env.js')
 var moduleChimp = require(process.env.PROJECTRUNPATH + '/project/support/chimp.js');
 // online mode is needed for test-init to download drivers
 moduleChimp.offline = true;
