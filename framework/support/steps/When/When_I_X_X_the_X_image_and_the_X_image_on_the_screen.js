57d9f743e4671d58c5fe3047570654ac91249c30
xywang68 <xywang68@gmail.com>
2020-02-04 19:28:11 -0800

    use simplified find_image calls

--- framework/support/steps/When/When_I_X_X_the_X_image_and_the_X_image_on_the_screen.js
+++ framework/support/steps/When/When_I_X_X_the_X_image_and_the_X_image_on_the_screen.js
@@ -4,22 +4,19 @@ module.exports = function() {
   this.When(/^I (click|hover|wave|shake|circle) (on|between) the "([^"]*)" image(?: and the "([^"]*)" image)? on the screen$/, {timeout: process.env.StepTimeoutInMS}, function (mouseAction, targetType, imageNameOne, imageNameTwo) {
     // re imageNameOne
     const parsedImageNameOne = parseExpectedText(imageNameOne);
-    const [imageFileNameOne, imageFileExtOne, imageSimilarityOne, imageSimilarityMaxOne] = this.fs_session.getTestImageParms(parsedImageNameOne);
+    const [imageFileNameOne, imageFileExtOne, imageSimilarityOne, maxSimilarityOrTextOne] = this.fs_session.getTestImageParms(parsedImageNameOne);
     const imagePathListOne = this.fs_session.globalSearchImageList(__dirname, imageFileNameOne, imageFileExtOne);
-    const imageScoreOne = this.lastImage && this.lastImage.imageNameOne == parsedImageNameOne ? this.lastImage.imageScoreOne : imageSimilarityOne;
     // re imageNameTwo
     const parsedImageNameTwo = parseExpectedText(imageNameTwo);
-    const [imageFileNameTwo, imageFileExtTwo, imageSimilarityTwo, imageSimilarityMaxTwo] = this.fs_session.getTestImageParms(parsedImageNameTwo);
+    const [imageFileNameTwo, imageFileExtTwo, imageSimilarityTwo, maxSimilarityOrTextTwo] = this.fs_session.getTestImageParms(parsedImageNameTwo);
     const imagePathListTwo = this.fs_session.globalSearchImageList(__dirname, imageFileNameTwo, imageFileExtTwo);
-    const imageScoreTwo = this.lastImage && this.lastImage.imageNameTwo == parsedImageNameTwo ? this.lastImage.imageScoreTwo : imageSimilarityTwo;
-    const imageWaitTime = process.env.imageWaitTime;
     switch (targetType) {
       case 'between':
         var locationOne, locationTwo;
         var targetLocation = {};
-        locationOne = JSON.parse(this.screen_session.screenFindImage(imagePathListOne, imageScoreOne, imageWaitTime));
+        locationOne = JSON.parse(this.screen_session.screenFindImage(imagePathListOne, imageSimilarityOne, maxSimilarityOrTextOne));
         expect(locationOne.length).not.toEqual(0, `can not locate the "${imageNameOne}" image on the screen`);
-        locationTwo = JSON.parse(this.screen_session.screenFindImage(imagePathListTwo, imageScoreTwo, imageWaitTime));
+        locationTwo = JSON.parse(this.screen_session.screenFindImage(imagePathListTwo, imageSimilarityTwo, maxSimilarityOrTextTwo));
         expect(locationTwo.length).not.toEqual(0, `can not locate the "${imageNameTwo}" image on the screen`);
         targetLocation.x = (locationOne[0].center.x + locationTwo[0].center.x) / 2;
         targetLocation.y = (locationOne[0].center.y + locationTwo[0].center.y) / 2;
@@ -59,27 +56,27 @@ module.exports = function() {
         var screenFindResult
         switch (mouseAction) {
           case "hover":
-            screenFindResult = JSON.parse(this.screen_session.screenHoverImage(imagePathListOne, imageScoreOne, imageWaitTime));
+            screenFindResult = JSON.parse(this.screen_session.screenHoverImage(imagePathListOne, imageSimilarityOne, maxSimilarityOrTextOne));
             break;
           case "click":
-            screenFindResult = JSON.parse(this.screen_session.screenClickImage(imagePathListOne, imageScoreOne, imageWaitTime));
+            screenFindResult = JSON.parse(this.screen_session.screenClickImage(imagePathListOne, imageSimilarityOne, maxSimilarityOrTextOne));
             break;
           case "wave":
-            screenFindResult = JSON.parse(this.screen_session.screenHoverImage(imagePathListOne, imageScoreOne, imageWaitTime));
+            screenFindResult = JSON.parse(this.screen_session.screenHoverImage(imagePathListOne, imageSimilarityOne, maxSimilarityOrTextOne));
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x,screenFindResult[0].center.y);
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x-5 ,screenFindResult[0].center.y);
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x+5 ,screenFindResult[0].center.y);
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x,screenFindResult[0].center.y);
             break;
           case "shake":
-            screenFindResult = JSON.parse(this.screen_session.screenHoverImage(imagePathListOne, imageScoreOne, imageWaitTime));
+            screenFindResult = JSON.parse(this.screen_session.screenHoverImage(imagePathListOne, imageSimilarityOne, maxSimilarityOrTextOne));
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x,screenFindResult[0].center.y);
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x ,screenFindResult[0].center.y-5);
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x ,screenFindResult[0].center.y+5);
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x,screenFindResult[0].center.y);
             break;
           case "circle":
-            screenFindResult = JSON.parse(this.screen_session.screenHoverImage(imagePathListOne, imageScoreOne, imageWaitTime));
+            screenFindResult = JSON.parse(this.screen_session.screenHoverImage(imagePathListOne, imageSimilarityOne, maxSimilarityOrTextOne));
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x,screenFindResult[0].center.y);
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x ,screenFindResult[0].center.y-5);
             this.screen_session.moveMouseSmooth(screenFindResult[0].center.x+5 ,screenFindResult[0].center.y);
