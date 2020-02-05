e97ebb0562eb46f1e3226dd272e4d74245f36068
xywang68 <xywang68@gmail.com>
2020-02-04 19:27:38 -0800

    use simplified find_image calls

--- framework/support/steps/Then/Then_I_should_see_the_X_image_on_the_screen.js
+++ framework/support/steps/Then/Then_I_should_see_the_X_image_on_the_screen.js
@@ -3,12 +3,11 @@ const parseExpectedText = require(FrameworkPath + '/framework/functions/common/p
 module.exports = function() {
   this.Then(/^I should(?: still)?( not)* see the "([^"]*)" image on the screen$/, {timeout: process.env.StepTimeoutInMS}, function (falseCase, imageName) {
     const parsedImageName = parseExpectedText(imageName);
-    const [imageFileName, imageFileExt, imageSimilarity, imageSimilarityMax] = this.fs_session.getTestImageParms(parsedImageName);
+    const [imageFileName, imageFileExt, imageSimilarity, maxSimilarityOrText] = this.fs_session.getTestImageParms(parsedImageName);
     const imagePathList = this.fs_session.globalSearchImageList(__dirname, imageFileName, imageFileExt);
     const imageScore = this.lastImage && this.lastImage.imageName == parsedImageName ? this.lastImage.imageScore : imageSimilarity;
-    const imageWaitTime = process.env.imageWaitTime;
     browser.pause(500);
-    var screenFindResult = JSON.parse(this.screen_session.findImageFromList('onScreen', imagePathList, imageScore, imageWaitTime));
+    var screenFindResult = JSON.parse(this.screen_session.screenFindAllImages(imagePathList, imageScore, maxSimilarityOrText));
     console.log(screenFindResult);
     if (falseCase) {
       expect(screenFindResult.length).toEqual(0, `expect image ${parsedImageName} not on the screen but found.`);
