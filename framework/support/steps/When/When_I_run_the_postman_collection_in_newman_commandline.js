d8867d10723a42d0d7b04b21085193bfaee06d70
xywang68 <xywang68@gmail.com>
2020-02-19 11:57:34 -0800

    use runCmd call directly

--- framework/support/steps/When/When_I_run_the_postman_collection_in_newman_commandline.js
+++ framework/support/steps/When/When_I_run_the_postman_collection_in_newman_commandline.js
@@ -1,6 +1,9 @@
 module.exports = function() {
     this.When(/^I run the postman collection in newman commandline$/, function () {
-        var result = this.cmdline_session.runNewman(this.postman_environment_file, this.postman_collection_file);
+        const newman_command = `newman run -e ${this.postman_environment_file} ${this.postman_collection_file}`;
+        console.log(newman_command);
+        const newman_result = this.cmdline_session.runCmd(newman_command);
+        let result = JSON.parse(newman_result);
         console.log(result.output);
         this.browser_session.displayMessage(browser, result.output);
         this.postman_result = result.output;
