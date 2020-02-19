0376873bab79a72ec5285e01ad496888e27576bd
xywang68 <xywang68@gmail.com>
2020-02-18 18:18:51 -0800

    fix loop bug

--- framework/functions/action/pressKeyTimes.js
+++ framework/functions/action/pressKeyTimes.js
@@ -1,6 +1,7 @@
 const screen_session = require('../../libs/screen_session');
 module.exports = (keyName, repeatTimes) => {
-    for (i=0; i<repeatTimes; i++) {
+    myRepeatTimes = repeatTimes || 1;
+    for (i=0; i<myRepeatTimes; i++) {
         screen_session.keyTap(keyName);
     }
 };
