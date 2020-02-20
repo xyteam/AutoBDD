766cc14d77cbe361a8008b2d1d39ad55c9749c93
xywang68 <xywang68@gmail.com>
2020-02-20 13:27:15 -0800

    refine check screen text to check text lines

--- framework/functions/action/typeText.js
+++ framework/functions/action/typeText.js
@@ -1,4 +1,7 @@
 const screen_session = require('../../libs/screen_session');
+const parseExpectedText = require('../common/parseExpectedText');
+
 module.exports = (text) => {
-    screen_session.typeString(text);
+    var parsedText = parseExpectedText(text);
+    screen_session.typeString(parsedText);
 };
