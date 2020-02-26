6b672455c0f1204fa0a1c5c27ba2ac2d6ed620b9
xywang68 <xywang68@gmail.com>
2020-02-25 17:47:14 -0800

    update findTargetImage.js

--- framework/scripts/findTargetImage.js
+++ framework/scripts/findTargetImage.js
@@ -11,19 +11,19 @@ const Pattern = xysikulixapi.Pattern;
 
 const argv = require('minimist')(process.argv.slice(2));
 const onArea = (argv.onArea != null && argv.onArea != 'undefined') ? argv.onArea : 'onScreen';
-const imagePath = (argv.imagePath != null && argv.imagePath != 'undefined') ? argv.imagePath : 'center';
+const imagePath = (argv.imagePath != null && argv.imagePath != 'undefined') ? argv.imagePath : 'Screen';
 const imageSimilarity = (argv.imageSimilarity != null && argv.imageSimilarity != 'undefined') ? argv.imageSimilarity : process.env.imageSimilarity || 0.8;
 const imageWaitTime = (argv.imageWaitTime != null && argv.imageWaitTime != 'undefined') ? argv.imageWaitTime : process.env.imageWaitTime || 1;
 const imageAction = (argv.imageAction != null && argv.imageAction != 'undefined') ? argv.imageAction : 'none';
-const maxSimilarityOrText = (argv.maxSimilarityOrText != null && argv.maxSimilarityOrText != 'undefined') ? argv.maxSimilarityOrText : 1;
+const maxSimOrText = (argv.maxSimOrText != null && argv.maxSimOrText != 'undefined') ? argv.maxSimOrText : 1;
 const imageMaxCount = (argv.imageMaxCount != null && argv.imageMaxCount != 'undefined') ? argv.imageMaxCount : 1;
 const notFoundStatus = {status: 'notFound'};
 
-const findImage = (onArea, imagePath, imageSimilarity, maxSimilarityOrText, imageWaitTime, imageAction, imageMaxCount) => {
+const findImage = (onArea, imagePath, imageSimilarity, maxSimOrText, imageWaitTime, imageAction, imageMaxCount) => {
   const myImageSimilarity = parseFloat(imageSimilarity);
   const myImageWaitTime = parseFloat(imageWaitTime);
-  const myImageText = isNaN(maxSimilarityOrText) ? maxSimilarityOrText : '';
-  const mySimilarityMax = (myImageText.length > 0) ? 1 : parseFloat(maxSimilarityOrText);
+  const myImageText = isNaN(maxSimOrText) ? maxSimOrText : '';
+  const mySimilarityMax = (myImageText.length > 0) ? 1 : parseFloat(maxSimOrText);
   const myImageMaxCount = imageMaxCount || 1;
 
   var findRegion;
@@ -67,13 +67,14 @@ const findImage = (onArea, imagePath, imageSimilarity, maxSimilarityOrText, imag
       default:
         const oneTarget = (new Pattern(imagePath)).similarSync(java.newFloat(myImageSimilarity));
         const find_results = findRegion.findAllSync(oneTarget);
+        const myRegex = new RegExp(myImageText, 'i');
         var matchCount = 0;
         while (matchCount < myImageMaxCount && find_results.hasNextSync()) {
           find_item = find_results.nextSync();
           returnItem.score = Math.floor(find_item.getScoreSync()*1000000)/1000000;
           returnItem.text = find_item.textSync().split('\n');
           [returnItem.location, returnItem.dimension, returnItem.center] = fillRectangleInfo(find_item);
-          if (returnItem.score >= myImageSimilarity && returnItem.score <= mySimilarityMax && returnItem.text.join('\n').includes(myImageText)) {
+          if (returnItem.score >= myImageSimilarity && returnItem.score <= mySimilarityMax && returnItem.text.join('\n').match(myRegex)) {
             matchCount += 1;
             find_item.highlight(0.1);
             returnArray.push(returnItem);
@@ -115,5 +116,5 @@ const findImage = (onArea, imagePath, imageSimilarity, maxSimilarityOrText, imag
   }
   return JSON.stringify(returnArray);
 };
-const findImage_result = findImage(onArea, imagePath, imageSimilarity, maxSimilarityOrText, imageWaitTime, imageAction, imageMaxCount);
+const findImage_result = findImage(onArea, imagePath, imageSimilarity, maxSimOrText, imageWaitTime, imageAction, imageMaxCount);
 console.log(`fidnImage_result: ${findImage_result}`);
