2ce98b2e7bb1f0562075bac5fb3bd23c9993e383
Xinyu Wang <xwang@varmour.com>
2020-02-28 19:28:52 -0800

    add define

--- framework/functions/action/pressKeyTimes.js
+++ framework/functions/action/pressKeyTimes.js
@@ -1,6 +1,6 @@
 const screen_session = require('../../libs/screen_session');
 module.exports = (keyName, repeatTimes) => {
-    myRepeatTimes = repeatTimes || 1;
+    const myRepeatTimes = repeatTimes || 1;
     const [myKeyModifier, myKeyName] = (keyName.includes('-')) ? keyName.split('-') : [null, keyName];
     for (i=0; i<myRepeatTimes; i++) {
         screen_session.keyTap(myKeyName, myKeyModifier);
