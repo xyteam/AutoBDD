691b83adc90ebe9ac4475d6ff11db0665c431a51
xywang68 <xywang68@gmail.com>
2020-03-11 17:00:14 -0700

    add new steps

--- framework/support/env.js
+++ framework/support/env.js
@@ -58,7 +58,10 @@ if (process.env.PLATFORM == 'Linux') {
         process.env.chromeDriverVersion = '79.0.3945.36';
         break;
       case / 80\./.test(process.env.chromeVersion):
-        process.env.chromeDriverVersion = '80.0.3987.16';
+        process.env.chromeDriverVersion = '80.0.3987.106';
+        break;
+      case / 81\./.test(process.env.chromeVersion):
+        process.env.chromeDriverVersion = '81.0.4044.20';
         break;
     }
     console.log('Chrome Driver ' + process.env.chromeDriverVersion)  
