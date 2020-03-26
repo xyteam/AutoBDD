67a31bf6b4050f9cf42d6f2bdbe41fd81c689263
xywang68 <xywang68@gmail.com>
2020-03-25 18:41:48 -0700

    update steps to display browser log

--- framework/support/steps/Then/Then_browser_console_log_X_level_count_should_not_exceed_X.js
+++ framework/support/steps/Then/Then_browser_console_log_X_level_count_should_not_exceed_X.js
@@ -1,11 +1,15 @@
 module.exports = function() {
-  this.Then(/^browser console log (SEVERE) level count should not exceed (\d+)$/,
-  function (logLevel, logCount) {
-    var targetLog = browser.log('browser').value.filter(log => log.level == logLevel);
-    process.env.LastBrowserLog = targetLog;
-    console.log(targetLog);
-    browser_session.displayMessage(browser, targetLog);
-    var expectedCount = parseInt(logCount);
-    expect(targetLog.length).not.toBeGreaterThan(expectedCount)
+  this.Then(/^(the last )*browser console log (SEVERE) level count should( not)* exceed (\d+)$/,
+  function (last, logLevel, falseCase, logCount) {
+    const targetLogArray = (last) ? JSON.parse(process.env.LastBrowserLog) : browser.log('browser').value.filter(log => log.level == logLevel);
+    process.env.LastBrowserLog = JSON.stringify(targetLogArray);
+    console.log(process.env.LastBrowserLog);
+    this.browser_session.displayMessage(browser, process.env.LastBrowserLog);
+    const expectedCount = parseInt(logCount);
+    if (falseCase) {
+      expect(targetLogArray.length).not.toBeGreaterThan(expectedCount);
+    } else {
+      expect(targetLogArray.length).toBeGreaterThan(expectedCount);
+    }
   });
 };
--- framework/support/steps/Then/Then_browser_console_log_should_not_contain_X_words.js
+++ framework/support/steps/Then/Then_browser_console_log_should_not_contain_X_words.js
@@ -1,13 +1,17 @@
 module.exports = function() {
-	this.Then(/^browser console log should not contain "([^"]*)" words$/, function (regexWords) {
-    var myRegexWords = regexWords.toLowerCase();
-    var anyRegexWords = 'failed|rejected|unhandled|unauthorized|error|invalid';
-    var msgRegex = (myRegexWords.indexOf('any error') >=0) ? RegExp(anyRegexWords) : RegExp(myRegexWords);
-    var targetLog = browser.log('browser').value.filter(log => msgRegex.test(log.message.toLowerCase()) === true);
-    process.env.LastBrowserLog = targetLog;
-    console.log(targetLog);
-    browser_session.displayMessage(browser, targetLog);
-    var targetLogCount = targetLog.length;
-    expect(targetLogCount).not.toBeGreaterThan(0)
+  this.Then(/^(the last )*browser console log should( not)* contain "([^"]*)" words$/,
+  function (last, falseCase, regexWords) {
+    const myRegexWords = regexWords.toLowerCase();
+    const anyRegexWords = 'failed|rejected|unhandled|unauthorized|error|invalid';
+    const msgRegex = (myRegexWords.indexOf('any error') >=0) ? RegExp(anyRegexWords) : RegExp(myRegexWords);
+    const targetLogArray = (last) ? JSON.parse(process.env.LastBrowserLog) : browser.log('browser').value.filter(log => msgRegex.test(log.message.toLowerCase()) === true);
+    process.env.LastBrowserLog = JSON.stringify(targetLogArray);
+    console.log(process.env.LastBrowserLog);
+    this.browser_session.displayMessage(browser, process.env.LastBrowserLog);
+    if (falseCase) {
+      expect(targetLogArray.length).not.toBeGreaterThan(0);
+    } else {
+      expect(targetLogArray.length).toBeGreaterThan(0);
+    }
   });
 };
