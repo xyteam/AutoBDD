57ca5068257422da22f8e4931fda1337abd07ff8
xywang68 <xywang68@gmail.com>
2020-04-03 12:37:01 -0700

    re-org to separate active and reference support files

--- framework/support/module_support_hooks.js
+++
@@ -1,4 +0,0 @@
-// if the framework is used
-if (process.env.FrameworkPath) {
-  require(process.env.FrameworkPath + '/framework/support/module_include_hooks.js');  
-}
--- test-projects/simple-test/test-something/support/hooks.js
+++ test-projects/simple-test/test-something/support/hooks.js
@@ -1,4 +1,4 @@
 // if the framework is used
 if (process.env.FrameworkPath) {
-  require(process.env.FrameworkPath + '/framework/support/module_include_hooks.js');  
+  require(process.env.FrameworkPath + '/framework/includes/module_include_hooks.js');  
 }
