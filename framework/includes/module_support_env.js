c6d3fe2b1ca500d508b396af4ef03e3b74892ac7
xywang68 <xywang68@gmail.com>
2020-04-03 12:43:03 -0700

    clean up

--- framework/includes/module_support_env.js
+++ framework/includes/module_support_env.js
@@ -6,8 +6,4 @@ var relativePathToProject = '../'.repeat(moduleDepth)
 require(relativePathToProject + 'project/support/env.js');
 // define module level Env vars here
 process.env.ThisModule = path.resolve().split(PROJECTNAME)[1].split('/')[1]
-// test env vars
-process.env.ROOTPATH = '/';
-process.env.LOGOID = '#logo';
-process.env.LOGO_FILENAME = 'chromeLogo';
-process.env.LOGO_TEXT = 'chrome';
+// test env vars here
