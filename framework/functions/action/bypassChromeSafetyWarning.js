0d6c71a22feb79ffdcf26674b0ce4e9353eeb90b
xwvArmour <50221398+xwvArmour@users.noreply.github.com>
2020-04-13 23:32:08 -0700

    improve bypass step (#131)

--- framework/functions/action/bypassChromeSafetyWarning.js
+++ framework/functions/action/bypassChromeSafetyWarning.js
@@ -1,4 +1,8 @@
-module.exports = () => {
+module.exports = (wait, ifPresent) =>{
+    if (wait) try {browser.waitForVisible('button=Advanced', 3000)} catch (e) {/*no-op*/};
+    if (!ifPresent) {
+      expect(browser.isExisting('button=Advanced')).toBe(true);
+    } 
     if (browser.isVisible('button=Advanced') || browser.isVisible('button=Back to safety')) {
         browser.click('button=Advanced');
         browser.click('#proceed-link');
