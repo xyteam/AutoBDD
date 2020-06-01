b887be541679132b89afc02009afefaaf7a37b56
Xinyu Wang <xwang@varmour.com>
2020-05-31 18:19:08 -0700

    add click to condition

---
+++ framework/functions/action/clickToCondition.js
@@ -0,0 +1,20 @@
+/**
+ * Perform an click action on the given element
+ * @param  {String}  clickElement  element to be clicked
+ * @param  {String}  clickCount    click up to number of times
+ * @param  {String}  checkElement  element to be checked
+ * @param  {String}  falseCase     fasle case
+ * @param  {String}  state         checked element state
+ */
+module.exports = (clickElement, clickCount, checkElement, falseCase, state) => {
+    var myClickCount = clickCount || 2;
+    const action = `is${state.charAt(0).toUpperCase()}${state.slice(1)}`;
+    var keepGoing = true;
+    do {
+      browser.click(clickElement);
+      myClickCount--;
+      browser.pause(300);
+      keepGoing = !browser[action](checkElement);
+      if (falseCase) keepGoing = !keepGoing;
+    } while (myClickCount > 0 && keepGoing);
+};
