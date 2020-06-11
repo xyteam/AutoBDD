2d7ced9dba1657fa8152281f0635d9d4e72f2e5f
xywang68 <xywang68@gmail.com>
2020-06-11 09:00:23 -0700

    take care of this.browser_session

--- framework/support/steps/Given/Given_I_have_a_java_cucumber_project_X.js
+++ framework/support/steps/Given/Given_I_have_a_java_cucumber_project_X.js
@@ -1,9 +1,11 @@
 const { Given } = require('cucumber');
+const FrameworkPath = process.env.FrameworkPath || process.env.HOME + '/Projects/AutoBDD';
+const browser_session = require(FrameworkPath + '/framework/libs/browser_session');
 Given(/^I have a java cucumber project "([^"]*)"$/, function (projectName) {
     this.javacucumber_project = projectName;
     var result = this.javacucumber_session.runMvnCleanProject(this.javacucumber_project);
     // console.log(result.output);
-    this.browser_session.displayMessage(browser, result.output);
+    browser_session.displayMessage(browser, result.output);
     expect(result.output).toContain('BUILD SUCCESS');
     expect(result.output).not.toContain('BUILD FAILURE');
     expect(result.exitcode).toBe(0);
