0d0b9f747d60555d18b79a07c441e6e47dc33c01
xywang68 <xywang68@gmail.com>
2020-06-12 12:35:13 -0700

    failFast to false

--- framework/configs/abdd_Linux_CH.js
+++ framework/configs/abdd_Linux_CH.js
@@ -203,7 +203,7 @@ exports.config = {
         // <boolean> invoke formatters without executing steps
         // dryRun: false,
         // <boolean> abort the run on first failure
-        failFast: true,
+        failFast: false,
         // <boolean> Enable this config to treat undefined definitions as
         // warnings
         ignoreUndefinedDefinitions: false,
