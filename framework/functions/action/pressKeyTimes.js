c65647d1285f08932a7a2d3548454f161b290fba
xywang68 <xywang68@gmail.com>
2020-06-24 23:31:47 -0700

    add toggle to press key function

--- framework/functions/action/pressKeyTimes.js
+++ framework/functions/action/pressKeyTimes.js
@@ -1,8 +1,18 @@
 const screen_session = require('../../libs/screen_session');
-module.exports = (keyName, repeatTimes) => {
+module.exports = (action, keyName, repeatTimes) => {
     const myRepeatTimes = repeatTimes || 1;
     const [myKeyModifier, myKeyName] = (keyName.includes('-')) ? keyName.split('-') : [null, keyName];
     for (i=0; i<myRepeatTimes; i++) {
-        screen_session.keyTap(myKeyName, myKeyModifier);
+        switch (action) {
+            case 'press':
+                screen_session.keyTap(myKeyName, myKeyModifier);
+                break;
+            case 'toggle up':
+                screen_session.keyToggle(myKeyName, 'up', myKeyModifier);
+                break;
+            case 'toggle down':
+                screen_session.keyToggle(myKeyName, 'down', myKeyModifier);
+                break;           
+        }
     }
 };
