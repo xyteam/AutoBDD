5253961868c9cd230fa1505e44b1fc0f511ae998
xywang68 <xywang68@gmail.com>
2020-06-27 18:35:16 -0700

    allow screen-## names

--- framework/support/steps/When/When_I_drag_X_and_drop_to_X.js
+++ framework/support/steps/When/When_I_drag_X_and_drop_to_X.js
@@ -4,13 +4,24 @@ const screen_session = require(FrameworkPath + '/framework/libs/screen_session')
 const fs_session = require(FrameworkPath + '/framework/libs/fs_session');
 const { When } = require('cucumber');
 When(/^I drag "([^"]*)" and drop to "([^"]*)"$/, function (imageNameOne, imageNameTwo) {
+    // re imageNameOne
     const parsedImageNameOne = parseExpectedText(imageNameOne);
     const [imageFileNameOne, imageFileExtOne, imageSimilarityOne, maxSimilarityOrTextOne] = fs_session.getTestImageParms(parsedImageNameOne);
-    const imagePathListOne = fs_session.globalSearchImageList(__dirname, imageFileNameOne, imageFileExtOne);
+    var imagePathListOne;
+    if (imageFileNameOne.includes('Screen')) {
+      imagePathListOne = imageFileNameOne;
+    } else {
+      imagePathListOne = fs_session.globalSearchImageList(__dirname, imageFileNameOne, imageFileExtOne);
+    }
     // re imageNameTwo
     const parsedImageNameTwo = parseExpectedText(imageNameTwo);
     const [imageFileNameTwo, imageFileExtTwo, imageSimilarityTwo, maxSimilarityOrTextTwo] = fs_session.getTestImageParms(parsedImageNameTwo);
-    const imagePathListTwo = fs_session.globalSearchImageList(__dirname, imageFileNameTwo, imageFileExtTwo);
+    var imagePathListTwo;
+    if (imageFileNameTwo.includes('Screen')) {
+      imagePathListTwo = imageFileNameTwo;
+    } else {
+      imagePathListTwo = fs_session.globalSearchImageList(__dirname, imageFileNameTwo, imageFileExtTwo);
+    }
 
     var locationOne = JSON.parse(screen_session.screenFindImage(imagePathListOne, imageSimilarityOne, maxSimilarityOrTextOne));
     expect(locationOne.length).not.toEqual(0, `can not locate the "${imageNameOne}" image on the screen`);
