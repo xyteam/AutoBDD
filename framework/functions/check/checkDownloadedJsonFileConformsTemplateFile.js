394b3eb7ba877c1b0d76db66fdf1392f04908c2e
xywang68 <xywang68@gmail.com>
2020-07-17 11:43:35 -0700

    use fs_session function to search test file

--- framework/functions/check/checkDownloadedJsonFileConformsTemplateFile.js
+++ framework/functions/check/checkDownloadedJsonFileConformsTemplateFile.js
@@ -4,29 +4,16 @@
  * @param  {String}   templateFileName    Template file name to check against
  */
 
-const glob = require('glob');
-const fs = require('fs');
 const Ajv = require('ajv');
-
-const FrameworkPath = process.env.FrameworkPath || process.env.HOME + '/Projects/AutoBDD';
-const FrameworkTestfilesPath = FrameworkPath + '/framework/testfiles';
-const ProjectPath = process.env.PROJECTRUNPATH;
-const TestDir = process.env.TestDir;
-const TestModule = process.env.TestModule;
-const ProjectTestfilesPath = `${ProjectPath}/${TestDir}/testfiles`;
-const ModuleTestfilesPath = `${ProjectPath}/${TestDir}/${TestModule}/testfiles`;
 const fs_session = require('../../libs/fs_session');
 const getDownloadDir = require('../common/getDownloadDir');
 const parseExpectedText = require('../common/parseExpectedText');
-
 module.exports = (jsonFileName, templateFileName) => {
     const jsonFile_extSplit = jsonFileName.split('.');
     const jsonFileExt = jsonFile_extSplit.length > 1 ? jsonFile_extSplit.pop() : null;
     const myJsonFileName = jsonFile_extSplit.join('.');
     const myJsonFilePath = glob.sync(getDownloadDir() + myJsonFileName + '.' + jsonFileExt)[0]; // we only process the first match
-    const templateFilePath = glob.sync(ModuleTestfilesPath + '/' + templateFileName)[0] 
-        || glob.sync(ProjectTestfilesPath + '/' + templateFileName)[0]
-        || glob.sync(FrameworkTestfilesPath + '/' + templateFileName)[0];
+    const templateFilePath = fs_session.getTestFileFullPath(templateFileName);
     console.log(templateFilePath);
     const jsonData = JSON.parse(fs_session.readJsonData(myJsonFilePath));
     const jsonSchemaOrigString = fs_session.readJsonData(templateFilePath);
