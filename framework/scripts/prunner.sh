749dfb3d9ddfc297e31e0da01ada8c4e45d15fda
xywang68 <xywang68@gmail.com>
2020-09-01 23:14:14 -0700

    smart calculate job thread number

--- framework/scripts/prunner.sh
+++ framework/scripts/prunner.sh
@@ -3,8 +3,11 @@
 # parse args - begin
 PARAMS=""
 RUN_OPTS=""
+JOBS_COUNT=""
 while (( "$#" )); do
   case "$1" in
+    ####################################################################
+    # Example Section
     # opts processing example code
     #   -a|--my-boolean-flag)
     #   MY_FLAG=0
@@ -23,6 +26,23 @@ while (( "$#" )); do
     #   echo "Error: Unsupported flag $1" >&2
     #   exit 1
     #   ;;
+    ####################################################################
+    # parallel jobs
+    -j=*|--jobs=*)
+      OptVal=${1#*=}
+      JOBS_COUNT=${OptVal}
+      shift
+      ;;
+    -j|--jobs)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        JOBS_COUNT=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    # cucumber tags
     -t=*|--cucumberOpts.tagExpression=*)
       OptVal=${1#*=}
       RUN_OPTS="${RUN_OPTS} --cucumberOpts.tagExpression=\"${OptVal}\""
@@ -36,8 +56,7 @@ while (( "$#" )); do
         echo "Error: Argument for $1 is missing" >&2
         exit 1
       fi
-      ;;
-    *) # preserve positional arguments
+      ;;    *) # preserve positional arguments
       PARAMS="$PARAMS $1"
       shift
       ;;
@@ -49,12 +68,16 @@ eval set -- "$PARAMS"
 # parse args - end
 
 CPU_COUNT=$(lscpu | grep CPU\(s\): | awk '{print $2}')
+if [[ "${JOBS_COUNT}" == "" ]]; then JOBS_COUNT=${CPU_COUNT}; fi
+if [[ "${JOBS_COUNT}" == *"/"* ]]; then JOBS_COUNT=`expr ${CPU_COUNT} \* ${JOBS_COUNT/\// \/ }`; fi
+if [[ "${JOBS_COUNT}" == *"-"* ]]; then JOBS_COUNT=`expr ${CPU_COUNT} ${JOBS_COUNT/-/ \- }`; fi
+if [[ "${JOBS_COUNT}" == *"-"* ]] || [[ "${JOBS_COUNT}" == "0" ]]; then JOBS_COUNT=1; fi
 SPEC_FILTER=${@:-.}
-SPEC_LIST=$(find ${SPEC_FILTER} -type f -name "*.feature" | sort | sed -e "s/^/--spec=/")
-echo running $(echo ${SPEC_LIST} | wc -w) feature files with ${CPU_COUNT} processes
+SPEC_LIST=$(find ${SPEC_FILTER} -type f -name "*.feature" | sort)
+echo running $(echo ${SPEC_LIST} | wc -w) feature files with ${JOBS_COUNT} processes
 echo ${SPEC_LIST} | tr " " "\n"
 rm -rf logs/*
 echo "to monitor progress"
-echo "tail -f `pwd`/logs/1/*/stdout"
+echo "tail -f `pwd`/logs/1/\\.*/stdout"
 echo
-time parallel --jobs=${CPU_COUNT} --results logs xvfb-runner.sh npx wdio abdd.js ${RUN_OPTS} {1} ${PARAMS} ::: ${SPEC_LIST}
+time parallel --jobs=${JOBS_COUNT} --results logs xvfb-runner.sh npx wdio abdd.js ${RUN_OPTS} --spec={1} ${PARAMS} ::: ${SPEC_LIST}
