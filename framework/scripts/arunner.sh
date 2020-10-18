2a3ed5fc86d067af27cccd17f0f633df9dccca26
xywang68 <xywang68@gmail.com>
2020-10-17 18:20:39 -0700

    add xvfb-run option

--- framework/scripts/arunner.sh
+++ framework/scripts/arunner.sh
@@ -2,4 +2,9 @@
 echo running feature files with 1 processes
 rm -rf logs/*
 echo
-time npx wdio abdd.js $@
+if [ "$1" == "-x" ]; then
+    shift
+    time xvfb-runner.sh npx wdio abdd.js $@
+else
+    time npx wdio abdd.js $@
+fi
