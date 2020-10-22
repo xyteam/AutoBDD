16d6a3f62dbf409c017a174f6a38648aa45784ed
xywang68 <xywang68@gmail.com>
2020-10-22 15:59:09 -0700

    optimize start-up scripts

--- .docker/autobdd-dev.startup.sh
+++ .docker/autobdd-dev.startup.sh
@@ -23,42 +23,46 @@ fi
 USER=${USER:-root}
 HOME=/root
 if [ "$USER" != "root" ]; then
+    # USER
     echo "* enable custom user: $USER"
     if [ "$HOSTOS" == "Linux" ]; then
         useradd --uid $USERID --gid $GROUPID --create-home --shell /bin/bash --groups adm,sudo $USER
     else
         useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
     fi
+    export HOME=/home/$USER
+
+    # PASSWORD
     if [ -z "$PASSWORD" ]; then
         echo "  set default password to \"ubuntu\""
         PASSWORD=ubuntu
     fi
-    export HOME=/home/$USER
-    sed -i "s|%USER%|$USER|" /etc/supervisor/conf.d/supervisord.conf
-    sed -i "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
     echo "$USER:$PASSWORD" | chpasswd
     unset PASSWORD
 
     # HOME
-    # cd /root && tar cf - ./Projects/AutoBDD/node_modules | (cd $HOME && tar xf -)
-    tar_dir="./Projects/AutoBDD/node_modules"
-    parallel "tar -cf - ${tar_dir}/{} | (cd $HOME && tar xf -)" ::: "$(ls -A1 ${tar_dir})"
-    cp -r /root/{.gtkrc-2.0,.asoundrc} ${HOME}
-    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
-    # prepare sshd
-    mkdir -p /run/sshd
-    mkdir -p $HOME/Projects
+    # user dirs and files
+    # cd /root; parallel "tar -cf - ./{} | (cd $HOME && tar xf -)" ::: "$(ls -A1 -I Projects -I.xvfb-locks .)"
+    cd /root; tar cf - $(ls -A1 -I Projects -I.xvfb-locks .) | (cd $HOME; tar xf -)
+    # in dev env we only want node_modules
+    # cd /root; parallel "tar -cf - Projects/AutoBDD/node_modules/{} | (cd $HOME && tar xf -)" ::: "$(ls -A1 Projects/AutoBDD/node_modules)"
+    cd /root; tar cf - Projects/AutoBDD/node_modules | (cd $HOME; tar xf -)
     mkdir -p $HOME/.config/pcmanfm/LXDE/
     ln -sf /usr/local/share/doro-lxde-wallpapers/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/
-    # update file permission inside docer
+    # update file permission inside docker
     if [ "$HOSTOS" == "Linux" ]; then
       chown -R $USERID:$GROUPID $HOME
     else
       chown -R $USER:$USER $HOME
     fi
-    # set bash_profile
-    cat /root/.bashrc >> $HOME/.bash_profile && chown $USER:$USER $HOME/.bash_profile
+
+    # OTHER
+    sed -i "s|%USER%|$USER|" /etc/supervisor/conf.d/supervisord.conf
+    sed -i "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
+    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+    mkdir -p /run/sshd
 fi
+
 # add display and npm settings to bash_profile
 cat >> $HOME/.bash_profile << END_bash_profile
 export DISPLAY=:1
@@ -66,6 +70,7 @@ npm config set script-shell /bin/bash
 cd ~/Projects/AutoBDD
 . .autoPathrc.sh
 END_bash_profile
+chown $USER:$USER $HOME/.bash_profile
 
 # start supervisord
 exec /bin/tini -- /usr/local/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
--- .docker/autobdd-run.startup.sh
+++ .docker/autobdd-run.startup.sh
@@ -3,37 +3,49 @@
 USER=${USER:-root}
 HOME=/root
 if [ "$USER" != "root" ]; then
+    # USER
     echo "* enable custom user: $USER"
     if [ "$HOSTOS" == "Linux" ]; then
         useradd --uid $USERID --gid $GROUPID --create-home --shell /bin/bash --groups adm,sudo $USER
     else
         useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
     fi
+    export HOME=/home/$USER
+
+    # PASSWORD
     if [ -z "$PASSWORD" ]; then
         echo "  set default password to \"ubuntu\""
         PASSWORD=ubuntu
     fi
-    export HOME=/home/$USER
     echo "$USER:$PASSWORD" | chpasswd
-    cp -r /root/{.gtkrc-2.0,.asoundrc} ${HOME}
-    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
     unset PASSWORD
-    # home folder
-    cd /root && tar cf - ./Projects | (cd $HOME && tar xf -)
-    # update file permission inside docer
+
+    # HOME
+    cd /root
+    # user dirs and files
+    # cd /root; parallel "tar -cf - ./{} | (cd $HOME && tar xf -)" ::: "$(ls -A1 -I Projects -I.xvfb-locks .)"
+    cd /root; tar cf - $(ls -A1 -I Projects -I.xvfb-locks .) | (cd $HOME; tar xf -)
+    # in run env we want the entire AutoBDD folder
+    # cd /root; parallel "tar -cf - Projects/AutoBDD/{} | (cd $HOME && tar xf -)" ::: "$(ls -A1 Projects/AutoBDD)"
+    cd /root; tar -cf - Projects/AutoBDD | (cd $HOME; tar xf -)
+    # update file permission inside docker
     if [ "$HOSTOS" == "Linux" ]; then
       chown -R $USERID:$GROUPID $HOME
     else
       chown -R $USER:$USER $HOME
     fi
-    # set bash_profile
-    cat /root/.bashrc >> $HOME/.bash_profile && chown $USER:$USER $HOME/.bash_profile
+
+    # OTHER
+    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+    mkdir -p /run/sshd
 fi
+
 # add npm settings to bash_profile
 cat >> $HOME/.bash_profile << END_bash_profile
 npm config set script-shell /bin/bash
 END_bash_profile
+chown $USER:$USER $HOME/.bash_profile
 
 # run test by taking autorun.py parameters
-sudo -E su $USER -lm -s /bin/bash -c "cd $HOME/Projects/AutoBDD && . .autoPathrc.sh && ./framework/scripts/autorunner.py $@"
+sudo -E su $USER -m -s /bin/bash -c "cd $HOME/Projects/AutoBDD && . .autoPathrc.sh && ./framework/scripts/autorunner.py $@"
 
