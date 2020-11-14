c294f69f641f3096c51e58249f24e0a30f4c3e63
xywang68 <xywang68@gmail.com>
2020-11-13 17:07:17 -0800

    auto-fill run parmeter

--- framework/scripts/arunner.sh
+++ framework/scripts/arunner.sh
@@ -2,9 +2,22 @@
 echo running feature files with 1 processes
 rm -rf logs/*
 echo
+
 if [ "$1" == "-x" ]; then
     shift
-    time xvfb-runner.sh npx wdio abdd.js $@
+    RUN_CMD="xvfb-runner.sh npx wdio abdd.js"
 else
-    time npx wdio abdd.js $@
+    RUN_CMD="npx wdio abdd.js"
 fi
+
+IFS=' ' read -r -a array <<< $@
+for element in "${array[@]}"
+do
+    if [[ "$element" == *".feature"* && "$element" != "--spec="* ]]; then
+        element="--spec=$element"
+    fi
+    RUN_ARGS="$RUN_ARGS $element"
+done
+
+echo $RUN_CMD $RUN_ARGS
+time $RUN_CMD $RUN_ARGS
