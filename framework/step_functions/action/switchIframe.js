7b1fccb14b527d7b608d092ed04c1c2d76a85c44
xywang68 <xywang68@gmail.com>
2020-11-14 00:30:59 -0800

    add last to iframe order

--- framework/step_functions/action/switchIframe.js
+++ framework/step_functions/action/switchIframe.js
@@ -9,18 +9,22 @@ module.exports = (index, name) => {
      * The index of the option to select
      * @type {Int}
      */
+    let iFrameElement, iFrameIndex;
+    let iFrameName = (name) ? parseExpectedText(name) : '';
+
+    if (iFrameName.length > 0) {
+        iFrameElement = `iframe[name="${iFrameName}"]`;
+    } else {
+        iFrameElement = 'iframe';
+    }
+
     if (index && index == 'parent') {
         browser.switchToParentFrame();
+    } else if (index && index == 'last') {
+        iFrameIndex = $$(iFrameElement).length -1;
     } else {
-        const iFrameIndex = (index) ? parseInt(index, 10) - 1 : 0;
-        const iFrameName = (name) ? parseExpectedText(name) : '';
-        var iFrameElement;
-        if (iFrameName.length > 0) {
-            iFrameElement = `iframe[name="${iFrameName}"]`;
-        } else {
-            iFrameElement = 'iframe';
-        }
+        iFrameIndex = (index) ? parseInt(index, 10) - 1 : 0;
         $(iFrameElement).waitForExist();
-        browser.switchToFrame(browser.$$(iFrameElement)[iFrameIndex]);
+        browser.switchToFrame($$(iFrameElement)[iFrameIndex]);
     }
 };
