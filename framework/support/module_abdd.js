99d6ae1e1a60bde45b8c488b342c08b9efe38760
xywang68 <xywang68@gmail.com>
2020-11-26 23:13:31 +0000

    add module_abdd.js

---
+++ framework/support/module_abdd.js
@@ -0,0 +1,16 @@
+// derive running env-vars
+const path = require('path');
+process.env.PROJECTBASE = process.env.PROJECTBASE || 'test-projects';
+process.env.BDD_PROJECT = process.env.BDD_PROJECT || path.resolve().split(process.env.PROJECTBASE)[1].split('/')[1]
+process.env.PROJECTRUNPATH = path.resolve().split(process.env.BDD_PROJECT)[0] + process.env.BDD_PROJECT
+process.env.TestDir = path.resolve().split(process.env.BDD_PROJECT)[1].split('/')[1];
+process.env.TestModule = path.resolve().split(process.env.BDD_PROJECT)[1].split('/')[2];
+// require module - project - framework env-vars
+require('./support/env.js');
+require(`${process.env.PROJECTRUNPATH}/${process.env.TestDir}/support/env.js`);
+if (process.env.FrameworkPath) require(process.env.FrameworkPath + '/framework/support/env.js');
+var moduleAbdd = require(`${process.env.PROJECTRUNPATH}/${process.env.TestDir}/support/abdd.js`);
+// add or modify abdd config attributes as necessary
+// consol.log(moduleAbdd.config);
+// moduleAbdd.config.baseUrl = process.env.WEB_URL;
+module.exports = moduleAbdd;
