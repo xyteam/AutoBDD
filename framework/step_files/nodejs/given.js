c2497016b3cf78d365d41ff97038cedb9e955c7e
xywang68 <xywang68@gmail.com>
2020-12-03 05:44:55 +0000

    move function

--- framework/step_files/nodejs/given.js
+++ framework/step_files/nodejs/given.js
@@ -3,34 +3,33 @@ const parseExpectedText = require(`${process.env.FrameworkPath}/framework/step_f
 
 const { Given } = require('cucumber');
 
-
-Given(/^(?::nodejs: )?I call (?:(framework|project|module) )?function "(.*)" with parameters "(.*)"(?: and assign ouput as "(.*)")?(?: in order to .*)?$/,
+Given(/^(?::nodejs: )?I call (?:(framework|project|module) )?function "([^"]*)?" with parameters "([^"]*)?"(?: and assign ouput as "([^"]*)?")?(?: in order to .*)?$/,
     (funcType, funcName, funcParms, varName) => {
-        const myParsedParms = parseExpectedText(funcParms)
-        let runFunc;
-        switch (funcType) {
-            case "framework":
-                runFunc = require(`${process.env.PROJECTRUNPATH}/step_functions/${funcName}`);
-                break;
-	    default:
-            case "project":
-                runFunc = require(`${process.env.PROJECTRUNPATH}/${process.env.TestDir}/functions/${funcName}`);
-                break;
-            case "module":
-                runFunc = require(`${process.env.PROJECTRUNPATH}/${process.env.TestDir}/${process.env.TestModule}/functions/${funcName}`);
-                break;
+            const myParsedParms = parseExpectedText(funcParms)
+            let runFunc;
+            switch (funcType) {
+                case "framework":
+                    runFunc = require(`${process.env.PROJECTRUNPATH}/step_functions/${funcName}`);
+                    break;
+                default:
+                case "project":
+                    runFunc = require(`${process.env.PROJECTRUNPATH}/${process.env.TestDir}/functions/${funcName}`);
+                    break;
+                case "module":
+                    runFunc = require(`${process.env.PROJECTRUNPATH}/${process.env.TestDir}/${process.env.TestModule}/functions/${funcName}`);
+                    break;
+            }
+            const [parm1, parm2, parm3, parm4, parm5, parm6, parm7, parm8, parm9, parm10] = myParsedParms.split(',').map(s => s.trim());
+            new Promise((resolve, reject) => {
+                const result = runFunc(parm1, parm2, parm3, parm4, parm5, parm6, parm7, parm8, parm9, parm10);
+                resolve(result);
+            }).then(output => {
+                if (varName) {
+                process.env[varName] = output;
+                console.log(`assigned "${process.env[varName]}" to ENV:${varName}`);    
+            }    
+            });
         }
-        const [parm1, parm2, parm3, parm4, parm5, parm6, parm7, parm8, parm9, parm10] = myParsedParms.split(',').map(s => s.trim());
-        console.log(parm1, parm2, parm3, parm4, parm5, parm6, parm7, parm8, parm9, parm10);
-        let output = new Promise((resolve, reject) => {
-            const result = runFunc(parm1, parm2, parm3, parm4, parm5, parm6, parm7, parm8, parm9, parm10);
-            resolve(result);
-        });
-        if (varName) {
-            process.env[varName] = output;
-            console.log(`assigned "${process.env[varName]}" to ENV:${varName}`);    
-        }
-    }
 );
 
 
