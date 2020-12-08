222ea66ccb77ea404baba5cc65bc07ec473500cd
xywang68 <xywang68@gmail.com>
2020-12-07 17:23:41 -0800

    rename script

---
+++ .docker/autobdd.root/root/enable_python2_support.sh
@@ -0,0 +1,12 @@
+# (autobdd does not need py2, run this script if your test project needs py2)
+    apt install -q -y --allow-unauthenticated --fix-missing --no-install-recommends \
+	    -o Dpkg::Options::="--force-confdef" \
+	    -o Dpkg::Options::="--force-confold" \
+        python2 \
+        libpython2.7-stdlib \
+        python-dev; \
+    curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py && \
+        python2 get-pip.py && \
+        rm -f get-pip.py; \
+    cd /root/Projects/AutoBDD && \
+        pip install -r requirement.txt;
