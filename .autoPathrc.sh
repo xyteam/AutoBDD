0f01e1d03eb8bdbc4143f7127746f761b8c5d62e
xywang68 <xywang68@gmail.com>
2020-12-07 17:26:02 -0800

    re-arrange

--- .autoPathrc.sh
+++ .autoPathrc.sh
@@ -2,9 +2,6 @@
 export FrameworkPath=$(pwd)
 PATH=${PATH}:${FrameworkPath}/framework/scripts
 
-# strip all node_modules/.bin in PATH
-PATH=$(echo "${PATH//:/$'\n'}" | grep -v node_modules\/.bin | paste -sd ":" -)
-
 # assign current node_modules/.bin to PATH
 if [ -f ./node_modules/.bin/npm-path ]; then
   export PATH=$(./node_modules/.bin/npm-path)
--- .docker/autobdd.root/root/.bash_profile
+++ .docker/autobdd.root/root/.bash_profile
@@ -1,5 +1,9 @@
 # add display and npm settings to bash_profile
 export DISPLAY=:1
 npm config set script-shell /bin/bash
+
+# strip all node_modules/.bin in PATH
+PATH=$(echo "${PATH//:/$'\n'}" | grep -v node_modules\/.bin | paste -sd ":" -)
+
 cd ~/Projects/AutoBDD
 . .autoPathrc.sh
