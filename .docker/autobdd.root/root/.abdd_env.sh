ca983d15afc9363e77fe7c039a5ba27e4fdd6495
xywang68 <xywang68@gmail.com>
2021-04-01 00:25:27 -0700

    update

--- .docker/autobdd.root/root/.abdd_env.sh
+++ .docker/autobdd.root/root/.abdd_env.sh
@@ -9,18 +9,3 @@ for dn in $(find $(pwd) -maxdepth 2 -type d -name node_modules); do
     if [[ ":$PATH:" != *":$dn/.bin:"* ]]; then PATH=${PATH}:$dn/.bin; fi
     export PATH
 done
-
-# e2e-test
-if [[ -d e2e-test ]]; then
-    cd e2e-test;
-        pip install -r requirement.txt;
-        npm install;
-    cd - > /dev/null;
-fi
-
-# py-test
-if [[ -d py-test ]]; then
-    cd py-test;
-        pip install -r requirement.txt;
-    cd - > /dev/null;
-fi
